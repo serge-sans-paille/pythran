dce72805a4d6a81c1b1a16320a7093387e6ea8a5
serge guelton <serge.guelton@telecom-bretagne.eu>
2012-11-19 17:34:38 +0100

    WIP++

---
+++ pythran/tests/test_euler.py
@@ -0,0 +1,36 @@
+import unittest
+import pythran
+import os
+from imp import load_dynamic
+
+class CompileTest(object):
+    def __init__(self, module_name):
+        self.module_name=module_name
+
+    def __call__(self, check_output=False):
+        module_path=os.path.join(os.path.dirname(__file__),"euler",self.module_name+".py")
+        print self.module_name
+
+        specs = { "solve": [] }
+        mod = pythran.cxx_generator(self.module_name, file(module_path).read(), specs)
+        pymod=load_dynamic(self.module_name, pythran.compile(os.environ.get("CXX","c++"), mod, check=False))
+        res = getattr(pymod,"solve")()
+
+        if check_output:
+            compiled_code=compile(file(module_path).read(),"","exec")
+            env={}
+            eval(compiled_code, env)
+            ref=eval("solve()",env)
+            if ref != res:
+              print ref, res
+
+class TestCase(unittest.TestCase):
+    pass
+
+import glob
+for f in glob.glob(os.path.join(os.path.dirname(__file__),"euler", "*.py")):
+    name=os.path.splitext(os.path.basename(f))[0]
+    setattr(TestCase,"test_"+name, CompileTest(name))
+
+if __name__ == '__main__':
+    unittest.main()

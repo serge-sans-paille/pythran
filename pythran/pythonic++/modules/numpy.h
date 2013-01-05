85a72e65ebb2d6086009d6d05fc5efd8cb9124c8
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2013-01-05 15:05:54 +0100

    [numpy] add function zeros and ones

--- pythran/pythonic++/modules/numpy.h
+++ pythran/pythonic++/modules/numpy.h
@@ -31,7 +31,7 @@ namespace pythonic {
             };
 
         template<class... T, class type>
-        core::ndarray<type,sizeof...(T)> build_array(core::list<type> const& prev_l, type const& val, T const& ...l)
+            core::ndarray<type,sizeof...(T)> build_array(core::list<type> const& prev_l, type const& val, T const& ...l)
             {
                 core::ndarray<type, sizeof...(l)> a({l...});
                 for(int i=0;i<prev_l.size(); i++)
@@ -40,7 +40,7 @@ namespace pythonic {
             }
 
         template<class... T, class type>
-        core::ndarray<typename finalType<type>::Type, depth<core::list<type>>::Value + sizeof...(T)> build_array(core::list<core::list<type> > const &prev_l, core::list<type> const& l, T const& ...s)
+            core::ndarray<typename finalType<type>::Type, depth<core::list<type>>::Value + sizeof...(T)> build_array(core::list<core::list<type> > const &prev_l, core::list<type> const& l, T const& ...s)
             {
                 core::list<type> accumul(0);
                 for(int i=0;i<prev_l.size(); i++)
@@ -50,12 +50,67 @@ namespace pythonic {
             }
 
         template<class type>
-        core::ndarray<typename finalType<type>::Type, depth<core::list<type>>::Value> array(core::list<type> const& l)
+            core::ndarray<typename finalType<type>::Type, depth<core::list<type>>::Value> array(core::list<type> const& l)
             {
                 return build_array(l, l[0], l.size());
             }
 
         PROXY(pythonic::numpy, array);
+
+        template<class... T>
+            core::ndarray<double, sizeof...(T)> build_cst_array(double val, T... t)
+            {
+                core::ndarray<double, sizeof...(t)> a({t...});
+                long size = 1;
+                for(int i=0;i<sizeof...(t);i++)
+                    size *= a.shape[i];
+                for(int i=0;i<size; i++)
+                    a(i)=val;
+                return a;
+            }
+
+        template<int N>
+            struct apply_to_tuple
+            {
+                template<typename... T, typename... S>
+                    static core::ndarray<double, sizeof...(T)> builder(int val, std::tuple<T...>& t, S... s)
+                    {
+                        return apply_to_tuple<N-1>::builder(val, t, std::get<N-1>(t), s...);
+                    }
+            };
+
+        template<>
+            struct apply_to_tuple<0>
+            {
+                template<typename... T, typename... S>
+                    static core::ndarray<double, sizeof...(T)> builder(int val, std::tuple<T...>& t, S... s)
+                    {
+                        return build_cst_array(val, s...);
+                    }
+            };
+
+#define CST_ARRAY(NAME, VAL)\
+        template<class... T>\
+            core::ndarray<double, sizeof...(T)> NAME(std::tuple<T...> t)\
+            {\
+                return apply_to_tuple<sizeof...(T)-1>::builder(VAL, t, std::get<sizeof...(T)-1>(t));\
+            }\
+\
+        core::ndarray<double,1> NAME(int size)\
+        {\
+            core::ndarray<double, 1> a({size});\
+            for(int i=0;i<size;i++)\
+                a(i)=VAL;\
+            return a;\
+        }
+
+        CST_ARRAY(zeros, 0)
+
+        PROXY(pythonic::numpy, zeros);
+
+        CST_ARRAY(ones, 1)
+
+        PROXY(pythonic::numpy, ones);
     }
 }
 

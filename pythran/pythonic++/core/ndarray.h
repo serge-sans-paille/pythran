2e77e3f6927499c776c72b5fce2d5c8c948da568
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2013-01-07 00:18:52 +0100

    [numpy] access to numpy array for N dimensions

--- pythran/pythonic++/core/ndarray.h
+++ pythran/pythonic++/core/ndarray.h
@@ -34,9 +34,13 @@ namespace  pythonic {
             struct ndarray
             {
                 impl::shared_ref< raw_array<T> > data;
+                size_t offset_data;
                 long shape[N];
 
-                ndarray(std::initializer_list<size_t> s)
+                //  types
+                typedef T value_type;
+
+                ndarray(std::initializer_list<size_t> s): offset_data(0)
                 {
                     size_t r = 1;
                     long * is = shape;
@@ -45,46 +49,69 @@ namespace  pythonic {
                     data = impl::shared_ref< raw_array<T> >(r);
                 }
 
-                ndarray(T* d, long const* shp, long const size)
+                ndarray(T* d, long const* shp, long const size): offset_data(0)
                 {
                     std::copy(shp, shp + N, shape);
                     data = impl::shared_ref< raw_array<T> >(size, d);
                 }
 
-                ndarray(): data(impl::no_memory()) {}
-                ndarray(core::ndarray<T,N>&& array): data(std::move(array.data))
+                ndarray(impl::shared_ref< raw_array<T> > const& d, size_t ofs, long const* shp): offset_data(ofs)
+                {
+                    std::copy(shp, shp + N, shape);
+                    data = impl::shared_ref< raw_array<T> >(d);
+                }
+
+                ndarray(): data(impl::no_memory()), offset_data(0) {}
+                ndarray(core::ndarray<T,N>&& array): data(std::move(array.data)), offset_data(array.offset_data)
                 {
                     std::copy(array.shape, array.shape + N, shape);
                 }
-                ndarray(const core::ndarray<T,N>& array): data(array.data)
+                ndarray(const core::ndarray<T,N>& array): data(array.data), offset_data(array.offset_data)
                 {
                     std::copy(array.shape, array.shape + N, shape);
                 }
 
                 template<class... Types>
-                    T& operator()(Types... t)
+                    T& operator()(Types const... t)
                     {
-                        return *at(data->data, t...);
+                        return *at(data->data + offset_data, t...);
                     }
 
                 template<class... Types>
-                    int offset(int t0, int t1, Types... tn)
+                    T& operator()(Types const... t) const
+                    {
+                        return *at(data->data + offset_data, t...);
+                    }
+
+                template<class... Types>
+                    size_t offset(int const t0, int const t1, Types const... tn)
                     {
                         return offset(t0 * shape[N - sizeof...(Types) - 1] + t1, tn...); 
                     }
 
-                long unsigned int offset(int t0, int t1)
+                size_t offset(int const t0, int const t1)
                 {
                     return t0 * shape[N-1] + t1;
                 }
 
-                T* at(T* from, long unsigned int t)
+                T* at(T* from, size_t const t)
                 {
-                    return from +t;
+                    return from + t;
                 }
 
                 template<class... Types>
-                    T* at(T* from, Types... tn)
+                    T* at(T* from, Types const... tn)
+                    {
+                        return at(from, offset(tn...));
+                    }
+
+                T* at(T* from, size_t const t) const
+                {
+                    return from + t;
+                }
+
+                template<class... Types>
+                    T* at(T* from, Types const... tn) const
                     {
                         return at(from, offset(tn...));
                     }

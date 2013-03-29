7eedb56c532e44206f25d8b8efd9a5a2ba9fd4d3
serge guelton <serge.guelton@telecom-bretagne.eu>
2013-03-29 22:52:23 +0100

    more ufunc

--- pythran/pythonic++/core/numpy_expr.h
+++ pythran/pythonic++/core/numpy_expr.h
@@ -24,6 +24,7 @@
 #include <nt2/include/functions/hypot.hpp>
 #include <nt2/include/functions/indeg.hpp>
 #include <nt2/include/functions/inrad.hpp>
+#include <nt2/include/functions/is_finite.hpp>
 #include <nt2/include/functions/is_inf.hpp>
 #include <nt2/include/functions/is_nan.hpp>
 #include <nt2/include/functions/ldexp.hpp>
@@ -34,6 +35,7 @@
 #include <nt2/include/functions/max.hpp>
 #include <nt2/include/functions/min.hpp>
 #include <nt2/include/functions/mod.hpp>
+#include <nt2/include/functions/nextafter.hpp>
 #include <nt2/include/functions/pow.hpp>
 #include <nt2/include/functions/sin.hpp>
 #include <nt2/include/functions/sinh.hpp>
@@ -95,8 +97,48 @@ namespace  pythonic {
                 }
 
             template<class T0, class T1>
-                auto minus(T0 const& t0, T1 const& t1) -> decltype(t0 - t1) {
-                    return t0 - t1;
+                auto left_shift(T0 const& t0, T1 const& t1) -> decltype(t0 << t1) {
+                    return t0 << t1;
+                }
+
+            template<class T0, class T1>
+                auto less(T0 const& t0, T1 const& t1) -> decltype(t0 < t1) {
+                    return t0 < t1;
+                }
+
+            template<class T0, class T1>
+                auto less_equal(T0 const& t0, T1 const& t1) -> decltype(t0 <= t1) {
+                    return t0 <= t1;
+                }
+
+            template<class T0, class T1>
+                auto logaddexp(T0 const& t0, T1 const& t1) -> decltype(nt2::log(nt2::exp(t0) + nt2::exp(t1))) {
+                    return nt2::log(nt2::exp(t0) + nt2::exp(t1));
+                }
+
+            template<class T0, class T1>
+                auto logaddexp2(T0 const& t0, T1 const& t1) -> decltype(nt2::log2(nt2::pow(T0(2),t0) + nt2::pow(T1(2),t1))) {
+                    return nt2::log2(nt2::pow(T0(2),t0) + nt2::pow(T1(2),t1));
+                }
+
+            template<class T0, class T1>
+                auto logical_and(T0 const& t0, T1 const& t1) -> decltype(t0 and t1) {
+                    return t0 and t1;
+                }
+
+            template<class T>
+                auto logical_not(T const& t) -> decltype(not t) {
+                    return not t;
+                }
+
+            template<class T0, class T1>
+                auto logical_or(T0 const& t0, T1 const& t1) -> decltype(t0 or t1) {
+                    return t0 or t1;
+                }
+
+            template<class T0, class T1>
+                auto logical_xor(T0 const& t0, T1 const& t1) -> decltype((t0 and not t1) or (t1 and not t0)) {
+                    return (t0 and not t1) or (t1 and not t0);
                 }
 
             template<class T0, class T1>
@@ -106,28 +148,31 @@ namespace  pythonic {
 
 
             template<class T >
-                auto negate(T const& t) -> decltype(-t) {
+                auto negative(T const& t) -> decltype(-t) {
                     return -t;
+                }
+
+            template<class T0, class T1>
+                auto not_equal(T0 const& t0, T1 const& t1) -> decltype(t0 != t1) {
+                    return t0 != t1;
+                }
+
+
+            template<class T0, class T1>
+                auto minus(T0 const& t0, T1 const& t1) -> decltype(t0 - t1) {
+                    return t0 - t1;
                 }
 
 
         }
     }
     namespace core {
-        PROXY(pythonic::numpy_expr::ops, multiply)
-#define NUMPY_BINARY_FUNC_NAME operator*
-#define NUMPY_BINARY_FUNC_SYM proxy::multiply
-#include "numpy_binary_expr.h"
 
             PROXY(pythonic::numpy_expr::ops, minus)
 #define NUMPY_BINARY_FUNC_NAME operator-
 #define NUMPY_BINARY_FUNC_SYM proxy::minus
 #include "numpy_binary_expr.h"
 
-            PROXY(pythonic::numpy_expr::ops, negate)
-#define NUMPY_UNARY_FUNC_NAME operator-
-#define NUMPY_UNARY_FUNC_SYM proxy::negate
-#include "numpy_unary_expr.h"
 
             PROXY(nt2, abs)
 #define NUMPY_UNARY_FUNC_NAME absolute
@@ -294,6 +339,7 @@ namespace  pythonic {
 #define NUMPY_BINARY_FUNC_NAME operator%
 #define NUMPY_BINARY_FUNC_SYM proxy::mod
 #include "numpy_binary_expr.h"
+
 #define NUMPY_BINARY_FUNC_NAME fmod
 #define NUMPY_BINARY_FUNC_SYM proxy::mod
 #include "numpy_binary_expr.h"
@@ -324,6 +370,15 @@ namespace  pythonic {
 #define NUMPY_BINARY_FUNC_SYM proxy::hypot
 #include "numpy_binary_expr.h"
 
+#define NUMPY_UNARY_FUNC_NAME invert
+#define NUMPY_UNARY_FUNC_SYM proxy::bitwise_not
+#include "numpy_unary_expr.h"
+
+            PROXY(nt2, is_finite)
+#define NUMPY_UNARY_FUNC_NAME isfinite
+#define NUMPY_UNARY_FUNC_SYM proxy::is_finite
+#include "numpy_unary_expr.h"
+
             PROXY(nt2, is_inf)
 #define NUMPY_UNARY_FUNC_NAME isinf
 #define NUMPY_UNARY_FUNC_SYM proxy::is_inf
@@ -333,6 +388,35 @@ namespace  pythonic {
 #define NUMPY_UNARY_FUNC_NAME isnan
 #define NUMPY_UNARY_FUNC_SYM proxy::is_nan
 #include "numpy_unary_expr.h"
+
+            PROXY(nt2, ldexp)
+#define NUMPY_BINARY_FUNC_NAME ldexp
+#define NUMPY_BINARY_FUNC_SYM proxy::ldexp
+#include "numpy_binary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, left_shift)
+#define NUMPY_BINARY_FUNC_NAME operator<<
+#define NUMPY_BINARY_FUNC_SYM proxy::left_shift
+#include "numpy_binary_expr.h"
+#define NUMPY_BINARY_FUNC_NAME left_shift
+#define NUMPY_BINARY_FUNC_SYM proxy::left_shift
+#include "numpy_binary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, less)
+#define NUMPY_BINARY_FUNC_NAME operator<
+#define NUMPY_BINARY_FUNC_SYM proxy::less
+#include "numpy_binary_expr.h"
+#define NUMPY_BINARY_FUNC_NAME less
+#define NUMPY_BINARY_FUNC_SYM proxy::less
+#include "numpy_binary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, less_equal)
+#define NUMPY_BINARY_FUNC_NAME operator<=
+#define NUMPY_BINARY_FUNC_SYM proxy::less_equal
+#include "numpy_binary_expr.h"
+#define NUMPY_BINARY_FUNC_NAME less_equal
+#define NUMPY_BINARY_FUNC_SYM proxy::less_equal
+#include "numpy_binary_expr.h"
 
             PROXY(nt2, log)
 #define NUMPY_UNARY_FUNC_NAME log
@@ -352,6 +436,100 @@ namespace  pythonic {
             PROXY(nt2, log2)
 #define NUMPY_UNARY_FUNC_NAME log2
 #define NUMPY_UNARY_FUNC_SYM proxy::log2
+#include "numpy_unary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, logaddexp)
+#define NUMPY_BINARY_FUNC_NAME logaddexp
+#define NUMPY_BINARY_FUNC_SYM proxy::logaddexp
+#include "numpy_binary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, logaddexp2)
+#define NUMPY_BINARY_FUNC_NAME logaddexp2
+#define NUMPY_BINARY_FUNC_SYM proxy::logaddexp2
+#include "numpy_binary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, logical_and)
+#define NUMPY_BINARY_FUNC_NAME operator&&
+#define NUMPY_BINARY_FUNC_SYM proxy::logical_and
+#include "numpy_binary_expr.h"
+
+#define NUMPY_BINARY_FUNC_NAME logical_and
+#define NUMPY_BINARY_FUNC_SYM proxy::logical_and
+#include "numpy_binary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, logical_not)
+#define NUMPY_UNARY_FUNC_NAME logical_not
+#define NUMPY_UNARY_FUNC_SYM proxy::logical_not
+#include "numpy_unary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, logical_or)
+#define NUMPY_BINARY_FUNC_NAME operator||
+#define NUMPY_BINARY_FUNC_SYM proxy::logical_or
+#include "numpy_binary_expr.h"
+
+#define NUMPY_BINARY_FUNC_NAME logical_or
+#define NUMPY_BINARY_FUNC_SYM proxy::logical_or
+#include "numpy_binary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, logical_xor)
+#define NUMPY_BINARY_FUNC_NAME logical_xor
+#define NUMPY_BINARY_FUNC_SYM proxy::logical_xor
+#include "numpy_binary_expr.h"
+
+#define NUMPY_BINARY_FUNC_NAME maximum
+#define NUMPY_BINARY_FUNC_SYM proxy::max
+#include "numpy_binary_expr.h"
+
+#define NUMPY_BINARY_FUNC_NAME minimum
+#define NUMPY_BINARY_FUNC_SYM proxy::max
+#include "numpy_binary_expr.h"
+
+#define NUMPY_BINARY_FUNC_NAME mod
+#define NUMPY_BINARY_FUNC_SYM proxy::mod
+#include "numpy_binary_expr.h"
+
+            // TODO modf
+
+        PROXY(pythonic::numpy_expr::ops, multiply)
+#define NUMPY_BINARY_FUNC_NAME operator*
+#define NUMPY_BINARY_FUNC_SYM proxy::multiply
+#include "numpy_binary_expr.h"
+#define NUMPY_BINARY_FUNC_NAME multiply
+#define NUMPY_BINARY_FUNC_SYM proxy::multiply
+#include "numpy_binary_expr.h"
+
+        PROXY(pythonic::numpy_expr::ops, negative)
+#define NUMPY_UNARY_FUNC_NAME operator-
+#define NUMPY_UNARY_FUNC_SYM proxy::negative
+#include "numpy_unary_expr.h"
+#define NUMPY_UNARY_FUNC_NAME negative
+#define NUMPY_UNARY_FUNC_SYM proxy::negative
+#include "numpy_unary_expr.h"
+
+            PROXY(nt2, nextafter)
+#define NUMPY_BINARY_FUNC_NAME nextafter
+#define NUMPY_BINARY_FUNC_SYM proxy::nextafter
+#include "numpy_binary_expr.h"
+
+            PROXY(pythonic::numpy_expr::ops, not_equal)
+#define NUMPY_BINARY_FUNC_NAME operator!=
+#define NUMPY_BINARY_FUNC_SYM proxy::not_equal
+#include "numpy_binary_expr.h"
+#define NUMPY_BINARY_FUNC_NAME not_equal
+#define NUMPY_BINARY_FUNC_SYM proxy::not_equal
+#include "numpy_binary_expr.h"
+
+            PROXY(nt2, pow)
+#define NUMPY_BINARY_FUNC_NAME power
+#define NUMPY_BINARY_FUNC_SYM proxy::pow
+#include "numpy_binary_expr.h"
+
+#define NUMPY_UNARY_FUNC_NAME rad2deg
+#define NUMPY_UNARY_FUNC_SYM proxy::indeg
+#include "numpy_unary_expr.h"
+
+#define NUMPY_UNARY_FUNC_NAME radians
+#define NUMPY_UNARY_FUNC_SYM proxy::inrad
 #include "numpy_unary_expr.h"
 
             PROXY(nt2, sin)

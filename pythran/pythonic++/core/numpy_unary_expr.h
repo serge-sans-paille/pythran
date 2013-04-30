226cbfebd6cd66f19dd453d286b37af46f2102ce
serge guelton <serge.guelton@telecom-bretagne.eu>
2013-04-30 17:29:26 +0200

    Extend numpy expression to slices, pass the validation

--- pythran/pythonic++/core/numpy_unary_expr.h
+++ pythran/pythonic++/core/numpy_unary_expr.h
@@ -9,6 +9,10 @@ template<class T, unsigned long N>
 numpy_uexpr<NUMPY_UNARY_FUNC_SYM, ndarray<T,N>> NUMPY_UNARY_FUNC_NAME(ndarray<T,N> const & self) {
     return numpy_uexpr<NUMPY_UNARY_FUNC_SYM, ndarray<T,N>>(self);
 }
+template<class T>
+numpy_uexpr<NUMPY_UNARY_FUNC_SYM, sliced_ndarray<T>> NUMPY_UNARY_FUNC_NAME(sliced_ndarray<T> const & self) {
+    return numpy_uexpr<NUMPY_UNARY_FUNC_SYM, sliced_ndarray<T>>(self);
+}
 template<class Op, class Arg0, class Arg1>
 numpy_uexpr<NUMPY_UNARY_FUNC_SYM, numpy_expr<Op, Arg0, Arg1>> NUMPY_UNARY_FUNC_NAME(numpy_expr<Op, Arg0, Arg1> const& self) {
     return numpy_uexpr<NUMPY_UNARY_FUNC_SYM, numpy_expr<Op, Arg0, Arg1> >(self);

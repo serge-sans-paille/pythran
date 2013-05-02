cddf02df275441671bd741b9b024f50019825b22
Mehdi Amini <mehdi.amini@silkan.com>
2013-05-02 00:34:14 -0700

    Fix itertools permutations
    The second argument was not well handled. The implementation is
    still by far not the most efficient, and not that clean, but
    conformance is improved.
    This commit fixes three unittests.

--- pythran/tests/test_itertools.py
+++ pythran/tests/test_itertools.py
@@ -161,15 +161,25 @@ def ifilter_with_nested_lambdas(N):
         self.run_test("def permutations_g(l0,a): from itertools import permutations; return sum(map(lambda (x,y) : x*y, permutations((y for x in l0 for y in xrange(x)),a)))", [0,1,2], 2, permutations_g=[[int],int])
 
     def test_next_permutations(self):
-        self.run_test("def next_permutations(n): from itertools import permutations ; x = permutations(n,2) ; next(x) ; return map(None, x)", range(5), next_permutations=[[int]])
+        self.run_test("def next_permutations(n):"
+                      "  from itertools import permutations ;"
+                      "  x = permutations(n,2) ;"
+                      "  next(x) ;"
+                      "  return map(None, x)",
+                      range(5),
+                      next_permutations=[[int]])
 
     def test_permutations(self):
-        self.run_test("def permutations_(l0,a): from itertools import permutations; return sum(map(lambda (x,y) : x*y, permutations(l0,a)))", [0,1,2,3,4,5], 2, permutations_=[[int],int])
-
-    def test_permutations_2(self):
         '''Test permutation without second arg'''
         self.run_test("def permutations_2_(l0): "
                       "  from itertools import permutations;"
                       "  return list(permutations(l0))",
                       [0, 1, 2, 3],
                       permutations_2_=[[int]])
+
+    def test_permutations_with_prefix(self):
+        self.run_test("def permutations_(l0,a):"
+                      "  from itertools import permutations;"
+                      "  return list(permutations(l0,a))",
+                      [0,1,2,3,4,5], 2,
+                      permutations_=[[int],int])

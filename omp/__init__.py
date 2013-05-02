serge-sans-paille/pythran

commit 0b41090984647de0e5b3228e871c26c57a005938

Fix gomp library dynamic loading issues
* bug introduce from commit : 4a4676258bfd47a7fbefc51644eb58ffc60ab6ad

omp/__init__.py
@@ -18,13 +18,13 @@ class omp(object):
         paths = omp.LD_LIBRARY_PATHS
 
         # Try to load find libgomp shared library using loader search dirs
-        libgomp_path = ctypes.util.find_library("libgomp")
+        libgomp_path = ctypes.util.find_library("gomp")
 
         # Try to use custom paths if lookup failed
         for path in paths:
             if libgomp_path:
                 break
-            libgomp_path = ctypes.util.find_library(path+"libgomp")
+            libgomp_path = ctypes.util.find_library(path+"gomp")
 
         if not libgomp_path:
             raise EnvironmentError("I can't find a shared library for libgomp,"

commit 8ad74d57b36863dcc5ba1ae3e091dddc56f1f702

OpenMP shared library loading, fix bug introduced in 0b41090
ctypes.util.find_library needs the lib prefix when using absolute
path.

omp/__init__.py
@@ -24,7 +24,7 @@ class omp(object):
         for path in paths:
             if libgomp_path:
                 break
-            libgomp_path = ctypes.util.find_library(path+"gomp")
+            libgomp_path = ctypes.util.find_library(path+"libgomp")
 
         if not libgomp_path:
             raise EnvironmentError("I can't find a shared library for libgomp,"
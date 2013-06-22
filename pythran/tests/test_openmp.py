a3eecb4866e9c4e3f8085926d07a697818e73609
serge guelton <serge.guelton@telecom-bretagne.eu>
2013-06-22 10:45:57 +0200

    Conditionnal OpenMP validation
    Disable OpenMP validation if OpenMP is not supported by the underlying
    compiler (e.g. clang++)

--- pythran/tests/test_openmp.py
+++ pythran/tests/test_openmp.py
@@ -1,13 +1,17 @@
 import unittest
 from test_env import TestFromDir
 import os
-
+import pythran
 
 class TestOpenMP(TestFromDir):
     path = os.path.join(os.path.dirname(__file__),"openmp")
 
-
-TestOpenMP.populate(TestOpenMP)
+# only activate OpenMP tests if the underlying compiler supports OpenMP
+try:
+    pythran.compile_cxxcode('#include <omp.h>')
+    TestOpenMP.populate(TestOpenMP)
+except pythran.CompileError:
+    pass
 
 
 if __name__ == '__main__':

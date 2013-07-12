fc73c8a4407975887573191dfd3be448a249e08c
Yuancheng <yuancheng.peng@telecom-bretagne.eu>
2013-07-12 16:24:48 +0200

    Add diffusion as test case
    * Pythran performs well on the pure python code but not on the numpy version.

---
+++ pythran/tests/cases/diffusion_pure_python.py
@@ -0,0 +1,25 @@
+# Reference: http://continuum.io/blog/the-python-and-the-complied-python
+#pythran export diffusePurePython(float [][], float [][], int)
+#runas import numpy as np;lx,ly=np.zeros(2**7,2**7);u=np.zeros([lx,ly],dtype=np.double);u[lx/2,ly/2]=1000.0;tempsU=np.zeros([lx,ly],dtype=np.double);diffusePurePython(u,tempU,500)
+
+import numpy as np
+
+
+def diffusePurePython(u, tempU, iterNum):
+    """
+    Apply nested iteration for the Forward-Euler Approximation
+    """
+    mu = .1
+    row = u.shape[0]
+    col = u.shape[1]
+
+    for n in range(iterNum):
+        for i in range(1, row - 1):
+            for j in range(1, col - 1):
+                tempU[i, j] = u[i, j] + mu * (
+                    u[i + 1, j] - 2 * u[i, j] + u[i - 1, j] +
+                    u[i, j + 1] - 2 * u[i, j] + u[i, j - 1])
+        for i in range(1, row - 1):
+            for j in range(1, col - 1):
+                u[i, j] = tempU[i, j]
+                tempU[i, j] = 0.0

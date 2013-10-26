89223abc12645834036060219e0af6f027a2167d
Mehdi Amini <mehdi.amini@silkan.com>
2013-10-26 16:02:01 -0700

    Code style

--- pythran/pythonic++/core/iterator.h
+++ pythran/pythonic++/core/iterator.h
@@ -2,55 +2,77 @@
 #define PYTHONIC_ITERATOR_H
 namespace pythonic {
 
-    template <class T>
-        struct generator_iterator : std::iterator<std::forward_iterator_tag, typename T::result_type> {
-            T the_generator;
-            generator_iterator() : the_generator() { the_generator.__generator_state = -1 ;} // this represents the end
-            generator_iterator(T const& a_generator) : the_generator(a_generator) {
+    template<class T>
+    struct generator_iterator: std::iterator<std::forward_iterator_tag, typename T::result_type> {
+        T the_generator;
+        generator_iterator() :
+                the_generator()
+        {
+            the_generator.__generator_state = -1; // this represents the end
+        }
+        generator_iterator(T const& a_generator) :
+                the_generator(a_generator)
+        {
+        }
+        generator_iterator& operator++()
+        {
+            try {
+                the_generator.next();
+            } catch (core::StopIteration const&) {
+                the_generator.__generator_state = -1;
             }
-            generator_iterator& operator++() {
-                try { the_generator.next(); }
-                catch(core::StopIteration const&) {
-                    the_generator.__generator_state = -1;
-                }
-                return *this;
-            }
-            typename T::result_type operator*() const {
-                 return *the_generator;
-            }
-            bool operator!=(generator_iterator<T> const & other) const {
-                assert( other.the_generator.__generator_state == -1);
-                return the_generator.__generator_state != other.the_generator.__generator_state ;
-            }
-            bool operator<(generator_iterator<T> const & other) const {
-                assert( other.the_generator.__generator_state == -1);
-                return the_generator.__generator_state != other.the_generator.__generator_state ;
-            }
-        };
+            return *this;
+        }
+        typename T::result_type operator*() const
+        {
+            return *the_generator;
+        }
+        bool operator!=(generator_iterator<T> const & other) const
+        {
+            assert(other.the_generator.__generator_state == -1);
+            return the_generator.__generator_state != other.the_generator.__generator_state;
+        }
+        bool operator<(generator_iterator<T> const & other) const
+        {
+            assert(other.the_generator.__generator_state == -1);
+            return the_generator.__generator_state != other.the_generator.__generator_state;
+        }
+    };
 
-    struct empty_iterator : std::iterator< std::forward_iterator_tag, int > {
+    struct empty_iterator: std::iterator<std::forward_iterator_tag, int> {
         // Empty iterator used, among other things, by empty_set
-        empty_iterator() {};
-        empty_iterator(empty_iterator const&) {};
-        bool operator==(empty_iterator const&) const{
+        empty_iterator()
+        {
+        }
+        empty_iterator(empty_iterator const&)
+        {
+        }
+        bool operator==(empty_iterator const&) const
+        {
             return true;
         }
-        bool operator!=(empty_iterator const&) const{
+        bool operator!=(empty_iterator const&) const
+        {
             return false;
         }
-        bool operator<(empty_iterator const&) const{
+        bool operator<(empty_iterator const&) const
+        {
             return false;
         }
-        empty_iterator& operator++(){
+        empty_iterator& operator++()
+        {
             return *this;
         }
-        empty_iterator& operator++(int){
+        empty_iterator& operator++(int)
+        {
             return *this;
         }
-        void operator*() const{
+        void operator*() const
+        {
             return;
         }
-        void operator->() const{
+        void operator->() const
+        {
             return;
         }
     };

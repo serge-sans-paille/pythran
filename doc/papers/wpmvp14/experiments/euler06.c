52cbe3d3666e5e99529a1213b53e5308844bf555
serge guelton <serge.guelton@telecom-bretagne.eu>
2013-11-22 18:02:43 +0100

    Intro: first XP

---
+++ doc/papers/wpmvp14/experiments/euler06.c
@@ -0,0 +1,8 @@
+unsigned int solve(unsigned int n) {
+  unsigned int a = 0, s = 0;
+  for(unsigned int i = 0; i< n; ++i) {
+    a += i;
+    s += i*i;
+  }
+  return a * a - s;
+}
---
+++ doc/papers/wpmvp14/experiments/euler06.py
@@ -0,0 +1,4 @@
+def solve(n):
+  r = range(1, n)
+  a = sum(r)
+  return a * a - sum(i*i for i in r)

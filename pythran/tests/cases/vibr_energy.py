da5cd7c89d8b5a182107e6664144c814c53e07a0
Pierrick Brunet <pierrick.brunet@inria.fr>
2014-01-28 14:29:20 +0100

    Fix bad runas for benchmark

--- pythran/tests/cases/vibr_energy.py
+++ pythran/tests/cases/vibr_energy.py
@@ -3,10 +3,10 @@
 #pythran export calculate_vibr_energy(float[], float[], int)
 #pythran export calculate_vibr_energy(float[], float[], float)
 #pythran export calculate_vibr_energy(float[], float[], float [])
-#runas import numpy as np ; a = np.sin(np.ones(1000000)) ; ba = np.cos(np.ones(1000000)) ; n = np.arange(1000000); calculate_vibr_energy(a, b, n)
-#runas import numpy as np ; a = np.sin(np.ones(1000000)) ; ba = np.cos(np.ones(1000000)) ; calculate_vibr_energy(a, b, 10)
-#runas import numpy as np ; a = np.sin(np.ones(1000000)) ; ba = np.cos(np.ones(1000000)) ; calculate_vibr_energy(a, b, 10.)
-#runas import numpy as np ; a = np.sin(np.ones(1000000)) ; ba = np.cos(np.ones(1000000)) ; n = np.arange(1000000, dtype=np.double); calculate_vibr_energy(a, b, n)
+#runas import numpy as np ; a = np.sin(np.ones(1000000)) ; b = np.cos(np.ones(1000000)) ; n = np.arange(1000000); calculate_vibr_energy(a, b, n)
+#runas import numpy as np ; a = np.sin(np.ones(1000000)) ; b = np.cos(np.ones(1000000)) ; calculate_vibr_energy(a, b, 10)
+#runas import numpy as np ; a = np.sin(np.ones(1000000)) ; b = np.cos(np.ones(1000000)) ; calculate_vibr_energy(a, b, 10.)
+#runas import numpy as np ; a = np.sin(np.ones(1000000)) ; b = np.cos(np.ones(1000000)) ; n = np.arange(1000000, dtype=np.double); calculate_vibr_energy(a, b, n)
 import numpy
 def calculate_vibr_energy(harmonic, anharmonic, i):
     return numpy.exp(-harmonic * i - anharmonic * (i ** 2))

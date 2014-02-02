fe7b5ce7e73b72a3eb0c64189bd726418359799b
coyotte508 <coyotte508@gmail.com>
2014-02-02 16:15:32 +0100

    Fix raw euler31 (list instead of tuple)

--- pythran/tests/euler_raw/euler31.py
+++ pythran/tests/euler_raw/euler31.py
@@ -8,7 +8,7 @@ It is possible to make P2 in the following way:
 How many different ways can P2 be made using any number of coins?
 '''
 
-coins = (1, 2, 5, 10, 20, 50, 100, 200)
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
 def balance(pattern): return sum(coins[x]*pattern[x] for x in xrange(0, len(pattern)))
 

33fe60f728440ecf5811cc850c20562235d85658
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2014-02-04 12:14:31 +0100

    Rework handling of Python comparison
    In case of multiple comparisons, previous implementations evaluated all
    arguments before doing the comparison. This is a bad behavior, as
    evaluation of boolean expression are done lazily.
    
    This improves the performance of the nqueens benchmarks a lot!

--- pythran/passes.py
+++ pythran/passes.py
@@ -1033,9 +1033,20 @@ class NormalizeCompare(Transformation):
     >>> node = pm.apply(NormalizeCompare, node)
     >>> print pm.dump(backend.Python, node)
     def foo(a):
-        return foo_compare0(0, (a + 1), 3)
-    def foo_compare0($0, $1, $2):
-        return ($0 < $1 < $2)
+        return foo_compare0(a)
+    def foo_compare0(a):
+        $0 = 0
+        $1 = (a + 1)
+        if ($0 < $1):
+            pass
+        else:
+            return 0
+        $2 = 3
+        if ($1 < $2):
+            pass
+        else:
+            return 0
+        return 1
     '''
 
     def visit_Module(self, node):
@@ -1052,24 +1063,40 @@ class NormalizeCompare(Transformation):
     def visit_Compare(self, node):
         node = self.generic_visit(node)
         if len(node.ops) > 1:
+            # in case we have more than one compare operator
+            # we generate an auxillary function
+            # that lazily evaluates the needed parameters
+            imported_ids = self.passmanager.gather(ImportedIds, node, self.ctx)
+            imported_ids = sorted(imported_ids)
+            binded_args = [ast.Name(i, ast.Load()) for i in imported_ids]
+
+            # name of the new function
             forged_name = "{0}_compare{1}".format(
                     self.prefix,
                     len(self.compare_functions)
                     )
-            binded_args = [node.left] + node.comparators
-            args = ast.arguments([ast.Name('${}'.format(i), ast.Param())
-                for i in range(1 + len(node.ops))],
-                None, None, [])
-            node.left = ast.Name('$0', ast.Load())
-            node.comparators = [ast.Name('${}'.format(i), ast.Load())
-                    for i in range(1, 1 + len(node.ops))]
-            forged_fdef = ast.FunctionDef(
-                    forged_name,
-                    args,
-                    [ast.Return(node)],
-                    [])
-            self.compare_functions.append(forged_fdef)
-            return ast.Call(ast.Name(forged_name, ast.Load()),
+
+            # call site
+            call = ast.Call(ast.Name(forged_name, ast.Load()),
                     binded_args, [], None, None)
+
+            # new function
+            arg_names = [ast.Name(i, ast.Param()) for i in imported_ids]
+            args = ast.arguments(arg_names, None, None, [])
+
+            body = []  # iteratively fill the body (yeah, feel your body!)
+            body.append(ast.Assign([ast.Name('$0', ast.Store())], node.left))
+            for i, exp in enumerate(node.comparators):
+                body.append(ast.Assign([ast.Name('${}'.format(i+1), ast.Store())], exp))
+                cond = ast.Compare(ast.Name('${}'.format(i), ast.Load()),
+                                   [node.ops[i]],
+                                   [ast.Name('${}'.format(i+1), ast.Load())])
+                body.append(ast.If(cond, [ast.Pass()], [ast.Return(ast.Num(0))]))
+            body.append(ast.Return(ast.Num(1)))
+
+            forged_fdef = ast.FunctionDef(forged_name, args, body, [])
+            self.compare_functions.append(forged_fdef)
+
+            return call
         else:
             return node

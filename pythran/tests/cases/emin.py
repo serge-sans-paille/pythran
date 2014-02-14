67bd6938a6db461fc69f97dbd56b8f35072d0df9
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2014-02-14 11:56:01 +0100

    Add two extra test cases to the codebase

---
+++ pythran/tests/cases/emin.py
@@ -0,0 +1,65 @@
+#from https://gist.github.com/andersx/6061586
+#runas: run()
+#pythran export run()
+
+# A simple energy minimization program that uses steepest descent 
+# and a force field to minimize the energy of water in internal coordinates.
+# Written by Jan H. Jensen, 2013
+
+
+def Eandg(rOH,thetaHOH):
+
+    """"
+    Arguments: (internal coordinates of the water molecule)
+
+    rOH            O-H bond distance
+    thetaHOH       H-O-H bond angle
+
+
+    Returns:
+
+    E              Molecular force field energy
+    grOH           O-H bond stretch gradient
+    grthetaHOH     H-O-H bond angle bend gradient 
+
+    
+    Force field parameters:
+
+    kOH            Harmonic force constant, O-H bond strech
+    rOHe           Equilibrium distance, O-H
+    kHOH           Harmonic angle bend force constant, H-O-H angle bend
+    thetaHOHe      Equilibrium angle, H-O-H
+
+    """
+
+    kOH = 50.0
+    rOHe = 0.95
+    kHOH = 50.0
+    thetaHOHe = 104.5
+
+
+    E = 2 * kOH * (rOH - rOHe)**2 + kHOH * (thetaHOH - thetaHOHe)**2
+    grOH = 2 * kOH * (rOH - rOHe)
+    grthetaHOH = 2 * kHOH * (thetaHOH - thetaHOHe)
+
+    return (E, grOH, grthetaHOH)
+
+def run():
+    c = 0.005
+    n_steps = 2000000
+
+    #starting geometry
+    rOH = 10.0
+    thetaHOH = 180.0
+
+
+
+    for i in range(n_steps):
+        (E,grOH,gthetaHOH) = Eandg(rOH,thetaHOH)
+        if (abs(grOH) >0.001/c or abs(gthetaHOH) > 0.01/c ):
+            rOH = rOH - c*grOH
+            thetaHOH = thetaHOH - c*gthetaHOH
+
+    converged = (abs(grOH) >0.001/c or abs(gthetaHOH) > 0.01/c )
+
+    return converged, E,rOH,thetaHOH
---
+++ pythran/tests/cases/rc4.py
@@ -0,0 +1,25 @@
+#from http://www.emoticode.net/python/python-implementation-of-rc4-algorithm.html
+#runas: data = "e"*100 ; key = "f"*3 ; rc4_crypt(e, f)
+#pythran export rc4_crypt(str, str)
+
+#RC4 Implementation
+def rc4_crypt( data , key ):
+    
+    S = range(256)
+    j = 0
+    out = []
+    
+    #KSA Phase
+    for i in range(256):
+        j = (j + S[i] + ord( key[i % len(key)] )) % 256
+        S[i] , S[j] = S[j] , S[i]
+    
+    #PRGA Phase
+    for char in data:
+        i = j = 0
+        i = ( i + 1 ) % 256
+        j = ( j + S[i] ) % 256
+        S[i] , S[j] = S[j] , S[i]
+        out.append(chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
+        
+    return ''.join(out)

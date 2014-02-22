480816610a9467875af9b53abb9ec363df224a67
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2014-02-22 15:05:54 +0100

    Fix sum along axis of an array expression
    Force the evaluation of the expression to an array.
    Validated by a new test case, along a few others.

---
+++ pythran/tests/cases/wdist.py
@@ -0,0 +1,16 @@
+#from http://stackoverflow.com/questions/19277244/fast-weighted-euclidean-distance-between-points-in-arrays/19277334#19277334
+#pythran export slow_wdist(float64 [][], float64 [][], float64[][])
+#runas import numpy as np ; A = np.arange(6.).reshape((2,3)) ; B =  np.arange(1,7.).reshape((2,3)) ; W = np.arange(2,8.).reshape((2,3)) ; slow_wdist(A,B,W)
+
+import numpy as np
+def slow_wdist(A, B, W):
+
+    k,m = A.shape
+    _,n = B.shape
+    D = np.zeros((m, n))
+
+    for ii in xrange(m):
+        for jj in xrange(n):
+            wdiff = (A[:,ii] - B[:,jj]) / W[:,ii]
+            D[ii,jj] = np.sqrt((wdiff**2).sum())
+    return D

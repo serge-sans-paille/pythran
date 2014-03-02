54d6d02b4882521c7c1530c0b8330973527c2cf9
coyotte508 <coyotte508@gmail.com>
2014-03-02 18:45:44 +0100

    test_env can capture output of __init__ function

--- pythran/tests/test_euler_raw.py
+++ pythran/tests/test_euler_raw.py
@@ -3,9 +3,14 @@ from test_env import TestFromDir
 import os
 import glob
 
+
 class TestEulerRaw(TestFromDir):
-    path = os.path.join(os.path.dirname(__file__),"euler_raw")
+    path = os.path.join(os.path.dirname(__file__), "euler_raw")
     files = glob.glob(os.path.join(path,"euler*.py"))
+
+    @staticmethod
+    def interface(name=None, file=None):
+        return {"__init__": []}
 
 TestEulerRaw.populate(TestEulerRaw)
 

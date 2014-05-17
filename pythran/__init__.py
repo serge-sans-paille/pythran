a419c389b4318ae66d0f847095df257a70f3d0eb
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2014-05-17 16:00:41 +0200

    Pythran 0.5 "PyData"

--- pythran/__init__.py
+++ pythran/__init__.py
@@ -31,7 +31,7 @@ Higher level entry points includes:
 
 '''
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 from toolchain import (generate_cxx, compile_cxxfile, compile_cxxcode,
                        compile_pythrancode, compile_pythranfile, test_compile,

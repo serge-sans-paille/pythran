4d19681dc35ec6137ddd534cee4bb07c6da9785b
Pierrick Brunet <pierrick.brunet@inria.fr>
2014-09-22 18:48:36 +0200

    Avoid argmin, argmax evaluation.
    * Test on argmin((a + 5) * 2) and array of 100.000.000 elts
    * old implem: 803 ms
    * new implem: 582 ms
    * CPython : 776 ms
    * Same on 2D array:
    * old implem: 790 ms
    * new implem: 536 ms
    * CPython : 783 ms

--- pythran/pythonic/numpy/argmin.hpp
+++ pythran/pythonic/numpy/argmin.hpp
@@ -9,18 +9,45 @@
 namespace pythonic {
 
     namespace numpy {
+        template<class I0, class T>
+            long _argmin(I0 begin, I0 end, T& min_elts, utils::int_<1>)
+            {
+                auto local_min_elts = std::min_element(begin, end);
+                if(*local_min_elts < min_elts) {
+                    min_elts = *local_min_elts;
+                    return local_min_elts - begin;
+                }
+
+                return -1;
+            }
+
+        template<class I0, size_t N, class T>
+            long _argmin(I0 begin, I0 end, T& min_elts, utils::int_<N>)
+            {
+                long current_pos = 0;
+                long current_minarg = 0;
+                for(; begin != end; ++begin){
+                    long v = _argmin((*begin).begin(), (*begin).end(), min_elts,
+                            utils::int_<N - 1>());
+                    if(v>=0)
+                        current_minarg = current_pos + v;
+                    current_pos += (*begin).size();
+                }
+                return current_minarg;
+            }
+
         template<class E>
-            long argmin(E&& expr) {
-                auto arr = asarray(expr);
-                long sz = arr.size();
-                if(not sz) 
+            long argmin(E const& expr) {
+                if(not expr.size()) 
                     throw types::ValueError("empty sequence");
-                return std::min_element(arr.fbegin(), arr.fend()) - arr.fbegin();
+                using elt_type = typename types::numpy_expr_to_ndarray<E>::T;
+                elt_type argmin_value = std::numeric_limits<elt_type>::max();;
+                return _argmin(expr.begin(), expr.end(), argmin_value,
+                               utils::int_<types::numpy_expr_to_ndarray<E>::N>());
             }
         PROXY(pythonic::numpy, argmin);
 
     }
-
 }
 
 #endif

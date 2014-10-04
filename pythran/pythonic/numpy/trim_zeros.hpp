b9da727b94814eeb7c9976f2a56523d3a44fb064
Pierrick Brunet <pierrick.brunet@inria.fr>
2014-10-04 23:38:58 +0200

    Rework trim_zeroes

--- pythran/pythonic/numpy/trim_zeros.hpp
+++ pythran/pythonic/numpy/trim_zeros.hpp
@@ -2,26 +2,27 @@
 #define PYTHONIC_NUMPY_TRIMZEROS_HPP
 
 #include "pythonic/utils/proxy.hpp"
-#include "pythonic/utils/numpy_conversion.hpp"
-#include "pythonic/types/ndarray.hpp"
+#include "pythonic/types/numpy_gexpr.hpp"
 
 namespace pythonic {
 
     namespace numpy {
         template<class T>
-            types::ndarray<T,1> trim_zeros(types::ndarray<T,1> const& expr, types::str const& trim = "fb")
+            types::numpy_gexpr<T, types::contiguous_slice> trim_zeros(T const& expr, types::str const& trim = "fb")
             {
+                static_assert(types::numpy_expr_to_ndarray<T>::N == 1,
+                              "Not implemented : trim_zeroes only works for 1D array");
+
                 int begin = 0;
                 int end = expr.size();
                 if(trim.find("f") != std::string::npos)
-                    begin = std::find_if(expr.fbegin(), expr.fend(), [](T i){return i!=0;}) - expr.fbegin();
+                    begin = std::find_if(expr.begin(), expr.end(),
+                                         [](typename types::numpy_expr_to_ndarray<T>::T i){return i!=0;}) - expr.begin();
                 if(trim.find("b") != std::string::npos)
-                    while(*(expr.fbegin() + --end) != 0);
-                types::ndarray<T,1> out(types::array<long,1>{{end - begin}}, __builtin__::None);
-                std::copy(expr.fbegin() + begin, expr.fbegin() + end, out.fbegin());
-                return out;
+                    while(*(expr.begin() + --end) != 0);
+                return types::numpy_gexpr<T, types::contiguous_slice>(expr, types::contiguous_slice(begin, end));
             }
-        NUMPY_EXPR_TO_NDARRAY0(trim_zeros)
+
             PROXY(pythonic::numpy, trim_zeros)
 
     }

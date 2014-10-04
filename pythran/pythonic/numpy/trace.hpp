868e64f5bd9333457d94422e4c18d7ea40b42bfb
Pierrick Brunet <pierrick.brunet@inria.fr>
2014-10-04 23:20:58 +0200

    Reimplementation for trace

--- pythran/pythonic/numpy/trace.hpp
+++ pythran/pythonic/numpy/trace.hpp
@@ -2,23 +2,36 @@
 #define PYTHONIC_NUMPY_TRACE_HPP
 
 #include "pythonic/utils/proxy.hpp"
-#include "pythonic/utils/numpy_conversion.hpp"
-#include "pythonic/types/ndarray.hpp"
+#include "pythonic/types/numexpr_to_ndarray.hpp"
 
 namespace pythonic {
 
     namespace numpy {
+
         template<class T>
-            T trace(types::ndarray<T,2> const& expr, int offset=0)
+            typename types::numpy_expr_to_ndarray<T>::T trace(T const& expr, int offset=0)
             {
-                T res = 0;
-                long size = std::min(expr.shape[0] + std::min(offset, 0), expr.shape[1] - std::max(0, offset));
-                for(int i=0; i<size; ++i)
-                    res += expr[i][i + offset];
+                static_assert(types::numpy_expr_to_ndarray<T>::N == 2,
+                              "Not Implemented : Trace for dimension != 2");
+
+                typename types::numpy_expr_to_ndarray<T>::T res = 0;
+                long y_offset = std::max(-offset, 0);
+                long x_offset = std::max(0, offset);
+                long size = std::min(expr.size() - y_offset,
+                                     expr.fast(0).size() - x_offset);
+                if(offset < 0)
+                    for(long i=0; i<size; i++)
+                        res += expr.fast(i + offset).fast(i);
+                else if(offset > 0)
+                    for(long i=0; i<size; i++)
+                        res += expr.fast(i).fast(i + offset);
+                else
+                    for(long i=0; i<size; i++)
+                        res += expr.fast(i).fast(i);
                 return res;
             }
-        NUMPY_EXPR_TO_NDARRAY0(trace)
-            PROXY(pythonic::numpy, trace)
+
+        PROXY(pythonic::numpy, trace)
 
     }
 

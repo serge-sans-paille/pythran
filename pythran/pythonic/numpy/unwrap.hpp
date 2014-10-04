fd7cf0514c23f1515e641200b57cb3eb1b297fc0
Pierrick Brunet <pierrick.brunet@inria.fr>
2014-10-05 00:06:34 +0200

    Add recursive version of numpy.unwrap

--- pythran/pythonic/numpy/unwrap.hpp
+++ pythran/pythonic/numpy/unwrap.hpp
@@ -2,39 +2,47 @@
 #define PYTHONIC_NUMPY_UNWRAP_HPP
 
 #include "pythonic/utils/proxy.hpp"
+#include "pythonic/utils/int_.hpp"
 #include "pythonic/types/ndarray.hpp"
 #include "pythonic/numpy/pi.hpp"
-#include "pythonic/numpy/asarray.hpp"
 
 #include <nt2/include/functions/max.hpp>
 
 namespace pythonic {
 
     namespace numpy {
+        template<class I0, class I1>
+            void _unwrap(I0 ibegin, I0 iend, I1 obegin, double discont, utils::int_<1>)
+            {
+                *obegin = *ibegin;
+                ++ibegin;
+                for(; ibegin != iend; ++ibegin, ++obegin)
+                {
+                    if(nt2::abs(*obegin - *ibegin) > discont)
+                        *(obegin + 1) = *ibegin + 2 * pi * int((*obegin - *ibegin) / (discont));
+                    else
+                        *(obegin + 1) = *ibegin;
+                }
+            }
+
+        template<class I0, class I1, size_t N>
+            void _unwrap(I0 ibegin, I0 iend, I1 obegin, double discont, utils::int_<N>)
+            {
+                for(; ibegin != iend; ++ibegin, ++obegin)
+                    _unwrap((*ibegin).begin(), (*ibegin).end(), (*obegin).begin(), discont, utils::int_<N - 1>());
+            }
+
         template<class E>
             types::ndarray<double, types::numpy_expr_to_ndarray<E>::N> unwrap(E const& expr, double discont = pi)
             {
                 discont = nt2::max(discont, pi);
                 types::ndarray<double, types::numpy_expr_to_ndarray<E>::N> out(expr.shape, __builtin__::None);
-                auto arr = asarray(expr);
-                auto iter = arr.fbegin();
-                auto out_iter = out.fbegin();
-                *out_iter++ = *iter++;
-                for(auto end = arr.fend() ; iter != end; ++iter, ++out_iter)
-                {
-                    auto val = *iter;
-                    if(nt2::abs(*(out_iter - 1) - val) > discont)
-                        *out_iter = val + 2*pi * int((*(out_iter - 1) - val) / (discont));
-                    else
-                        *out_iter = val;
-                }
+                _unwrap(expr.begin(), expr.end(), out.begin(), discont, utils::int_<types::numpy_expr_to_ndarray<E>::N>());
                 return out;
             }
 
         PROXY(pythonic::numpy, unwrap)
-
     }
-
 }
 
 #endif

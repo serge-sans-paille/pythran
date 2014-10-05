1be5d4b487d4ea9ad8875fb1e668af62fbcf43c8
Pierrick Brunet <pierrick.brunet@inria.fr>
2014-10-05 03:30:38 +0200

    Forward declval to reshape avoid array evaluation in swapaxes function

--- pythran/pythonic/numpy/swapaxes.hpp
+++ pythran/pythonic/numpy/swapaxes.hpp
@@ -6,17 +6,19 @@
 namespace pythonic {
 
     namespace numpy {
-        template<class T, size_t N>
-            types::ndarray<T,N> swapaxes(types::ndarray<T,N> const & a, int axis1, int axis2)
+        template<class T>
+            auto swapaxes(T && a, int axis1, int axis2)
+            -> decltype(_transpose(std::forward<T>(a),
+                        std::declval<long [types::numpy_expr_to_ndarray<typename std::remove_cv<typename std::remove_reference<T>::type>::type>::N]>()))
             {
+                constexpr long N = types::numpy_expr_to_ndarray<typename std::remove_cv<typename std::remove_reference<T>::type>::type>::N;
                 long t[N];
                 for(unsigned long i = 0; i<N; ++i)
                     t[i] = i;
                 std::swap(t[axis1], t[axis2]);
-                return _transpose(a, t);
+                return _transpose(std::forward<T>(a), t);
             }
 
-        NUMPY_EXPR_TO_NDARRAY0(swapaxes);
         PROXY(pythonic::numpy, swapaxes);
 
     }

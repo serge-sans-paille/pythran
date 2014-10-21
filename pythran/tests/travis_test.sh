10983362f0082e275779d19f22f122d7c3d1a660
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2014-10-21 23:16:42 +0200

    Decrease load for travis run

--- pythran/tests/travis_test.sh
+++ pythran/tests/travis_test.sh
@@ -3,5 +3,5 @@ then
   PYTHONPATH=$PWD:$PYTHONPATH py.test pythran --pep8 -m pep8;
 else
   printf "[user]\ncxx=$CXX\ncxxflags=$CXXFLAGS\n" > ~/.pythranrc;
-  OMP_NUM_THREADS=4 PYTHONPATH=$PWD:$PYTHONPATH py.test -n 2 -v $TESTCASE;
+  OMP_NUM_THREADS=4 PYTHONPATH=$PWD:$PYTHONPATH py.test -n 1 -v $TESTCASE;
 fi

c4a22867bc6e2da876b9d3d97b3adbea58e31b34
xantares <xantares09@hotmail.com>
2014-11-13 20:03:18 +0100

    Revert "Revert "use relative imports""
    This reverts commit 762af2dcae875d4e5e006d02f5aeb0945a5b6d7c.

--- pythran/analyses/__init__.py
+++ pythran/analyses/__init__.py
@@ -9,34 +9,34 @@ into
 import analyses.Foo
 """
 
-from aliases import Aliases, StrictAliases
-from ancestors import Ancestors
-from argument_effects import ArgumentEffects
-from argument_read_once import ArgumentReadOnce
-from ast_matcher import ASTMatcher, AST_any, AST_or, Placeholder, Check
-from bounded_expressions import BoundedExpressions
-from cfg import CFG
-from constant_expressions import ConstantExpressions
-from dependencies import Dependencies
-from global_declarations import GlobalDeclarations
-from global_effects import GlobalEffects
-from globals_analysis import Globals
-from has_break import HasBreak
-from has_continue import HasContinue
-from identifiers import Identifiers
-from imported_ids import ImportedIds
-from is_assigned import IsAssigned
-from lazyness_analysis import LazynessAnalysis
-from literals import Literals
-from local_declarations import LocalDeclarations
-from locals_analysis import Locals
-from node_count import NodeCount
-from optimizable_comprehension import OptimizableComprehension
-from ordered_global_declarations import OrderedGlobalDeclarations
-from parallel_maps import ParallelMaps
-from potential_iterator import PotentialIterator
-from pure_expressions import PureExpressions
-from scope import Scope
-from use_def_chain import UseDefChain
-from use_omp import UseOMP
-from yield_points import YieldPoints
+from .aliases import Aliases, StrictAliases
+from .ancestors import Ancestors
+from .argument_effects import ArgumentEffects
+from .argument_read_once import ArgumentReadOnce
+from .ast_matcher import ASTMatcher, AST_any, AST_or, Placeholder, Check
+from .bounded_expressions import BoundedExpressions
+from .cfg import CFG
+from .constant_expressions import ConstantExpressions
+from .dependencies import Dependencies
+from .global_declarations import GlobalDeclarations
+from .global_effects import GlobalEffects
+from .globals_analysis import Globals
+from .has_break import HasBreak
+from .has_continue import HasContinue
+from .identifiers import Identifiers
+from .imported_ids import ImportedIds
+from .is_assigned import IsAssigned
+from .lazyness_analysis import LazynessAnalysis
+from .literals import Literals
+from .local_declarations import LocalDeclarations
+from .locals_analysis import Locals
+from .node_count import NodeCount
+from .optimizable_comprehension import OptimizableComprehension
+from .ordered_global_declarations import OrderedGlobalDeclarations
+from .parallel_maps import ParallelMaps
+from .potential_iterator import PotentialIterator
+from .pure_expressions import PureExpressions
+from .scope import Scope
+from .use_def_chain import UseDefChain
+from .use_omp import UseOMP
+from .yield_points import YieldPoints

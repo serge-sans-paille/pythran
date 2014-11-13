762af2dcae875d4e5e006d02f5aeb0945a5b6d7c
xantares <xantares09@hotmail.com>
2014-11-13 18:53:54 +0100

    Revert "use relative imports"
    This reverts commit 5a9b3289d6bb6eeb4dbd6c1f5c1416e3f9c48f37.

--- pythran/optimizations/__init__.py
+++ pythran/optimizations/__init__.py
@@ -10,14 +10,14 @@ into
 import optimisations.xxxxx
 """
 
-from .constant_folding import ConstantFolding
-from .dead_code_elimination import DeadCodeElimination
-from .forward_substitution import ForwardSubstitution
-from .gen_exp_to_imap import GenExpToImap
-from .iter_transformation import IterTransformation
-from .list_comp_to_genexp import ListCompToGenexp
-from .list_comp_to_map import ListCompToMap
-from .loop_full_unrolling import LoopFullUnrolling
-from .square import Square
-from .pattern_transform import PatternTransform
-from .range_loop_unfolding import RangeLoopUnfolding
+from constant_folding import ConstantFolding
+from dead_code_elimination import DeadCodeElimination
+from forward_substitution import ForwardSubstitution
+from gen_exp_to_imap import GenExpToImap
+from iter_transformation import IterTransformation
+from list_comp_to_genexp import ListCompToGenexp
+from list_comp_to_map import ListCompToMap
+from loop_full_unrolling import LoopFullUnrolling
+from square import Square
+from pattern_transform import PatternTransform
+from range_loop_unfolding import RangeLoopUnfolding
--- pythran/transformations/__init__.py
+++ pythran/transformations/__init__.py
@@ -11,19 +11,19 @@ import transformations.xxxxx
 """
 
 
-from .expand_builtins import ExpandBuiltins
-from .expand_import_all import ExpandImportAll
-from .expand_imports import ExpandImports
-from .extract_top_level_stmts import ExtractTopLevelStmts
-from .false_polymorphism import FalsePolymorphism
-from .normalize_compare import NormalizeCompare
-from .normalize_exception import NormalizeException
-from .normalize_identifiers import NormalizeIdentifiers
-from .normalize_method_calls import NormalizeMethodCalls
-from .normalize_return import NormalizeReturn
-from .normalize_tuples import NormalizeTuples
-from .remove_comprehension import RemoveComprehension
-from .remove_lambdas import RemoveLambdas
-from .remove_nested_functions import RemoveNestedFunctions
-from .unshadow_parameters import UnshadowParameters
-from .remove_named_arguments import RemoveNamedArguments
+from expand_builtins import ExpandBuiltins
+from expand_import_all import ExpandImportAll
+from expand_imports import ExpandImports
+from extract_top_level_stmts import ExtractTopLevelStmts
+from false_polymorphism import FalsePolymorphism
+from normalize_compare import NormalizeCompare
+from normalize_exception import NormalizeException
+from normalize_identifiers import NormalizeIdentifiers
+from normalize_method_calls import NormalizeMethodCalls
+from normalize_return import NormalizeReturn
+from normalize_tuples import NormalizeTuples
+from remove_comprehension import RemoveComprehension
+from remove_lambdas import RemoveLambdas
+from remove_nested_functions import RemoveNestedFunctions
+from unshadow_parameters import UnshadowParameters
+from remove_named_arguments import RemoveNamedArguments

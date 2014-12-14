084818aabbd3f0631f1852966031b8e68406c59e
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2014-12-14 16:15:16 +0100

    Fix bug in sliced ndarray to Pythran conversion
    Avoid null pointer dereferencement, thanks to nbecker for the bug report!
    
    This fixes #371

---
+++ pythran/tests/cases/clip.py
@@ -0,0 +1,23 @@
+#pythran export clip(complex128[], float64)
+#pythran export clip(complex128[::], float64)
+#runas import numpy as np ; a = np.arange(2, dtype=complex); clip(a, .5), clip(a[2::4], .5)
+#import numpy as np
+#from timeit import timeit
+
+import numpy as np
+
+def limit1 (x, epsilon=1e-6):
+    if abs(x) < epsilon:
+        return 0
+    else:
+        return x / abs(x)
+
+def clip (z, _max):
+    out = np.empty (z.shape, dtype=z.dtype)
+    for i in range (len(z)):
+        if abs(z[i]) > _max:
+            out[i] = limit1 (z[i]) * _max
+        else:
+            out[i] = z[i]
+
+    return out

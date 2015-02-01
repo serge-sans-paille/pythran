bac15c8ae27234545e64603e1ff252b8df734f63
Pierrick Brunet <pierrick.brunet@inria.fr>
2015-02-01 20:37:05 +0100

    Factorisation of type traits

--- pythran/pythonic/types/traits.hpp
+++ pythran/pythonic/types/traits.hpp
@@ -21,89 +21,61 @@ namespace pythonic {
                 typedef T type;
             };
 
-        /* trait to check if a type is iterable*/
-        template<typename T>
-            struct is_iterable
-            {
-                typedef char yes;
-                typedef struct { char _[2]; } no;
-
-                template <class C> static yes _test(typename C::iterator*);
-                template <class C> static no _test(...);
-                static const bool value = sizeof( _test<typename std::remove_reference<T>::type>(nullptr)) == sizeof(yes);
+#define MEMBER_TYPE_TRAIT(check_struct, member)\
+        template<typename T>\
+            struct check_struct\
+            {\
+                typedef char yes;\
+                typedef struct { char _[2]; } no;\
+                template <class C> static yes _test(typename C::member*);\
+                template <class C> static no _test(...);\
+                static const bool value = sizeof( _test<typename std::remove_reference<T>::type>(nullptr)) == sizeof(yes);\
             };
+
+#define MEMBER_ATTR_TRAIT(check_struct, member)\
+        template<typename T>\
+            struct check_struct\
+            {\
+                typedef char yes;\
+                typedef struct { char _[2]; } no;\
+                template <class C> static yes _test(decltype(&C::member));\
+                template <class C> static no _test(...);\
+                static const bool value = sizeof( _test<typename std::remove_reference<T>::type>(nullptr)) == sizeof(yes);\
+            };
+
+        /* trait to check if a type is iterable*/
+        MEMBER_TYPE_TRAIT(is_iterable, iterator);
 
         /* trait to check if a type is callable */
-        template<typename T>
-            struct is_callable
-            {
-                typedef char yes;
-                typedef struct { char _[2]; } no;
-
-                template <class C> static yes _test(typename C::callable*);
-                template <class C> static no _test(...);
-                static const bool value = sizeof( _test<T>(nullptr)) == sizeof(yes);
-            };
+        MEMBER_TYPE_TRAIT(is_callable, callable);
 
         /* trait to check if a type is pure */
-        template<typename T>
-            struct is_pure
-            {
-                typedef char yes;
-                typedef struct { char _[2]; } no;
-
-                template <class C> static yes _test(typename C::pure*);
-                template <class C> static no _test(...);
-                static const bool value = sizeof( _test<T>(nullptr)) == sizeof(yes);
-            };
+        MEMBER_TYPE_TRAIT(is_pure, pure);
 
         /* trait to check if the type has a size member */
-        template<typename T>
-            struct has_size
-            {
-                typedef char yes;
-                typedef struct { char _[2]; } no;
-
-                template <class C> static yes _test(decltype(&C::size));
-                template <class C> static no _test(...);
-                static const bool value = sizeof( _test<T>(nullptr)) == sizeof(yes);
-            };
+        MEMBER_ATTR_TRAIT(has_size, size);
 
         /* trait to check if the type has a contains member */
-        template<typename T, typename V>
-            struct has_contains
-            {
-                typedef char yes;
-                typedef struct { char _[2]; } no;
-
+        template<typename T, class V>\
+            struct has_contains\
+            {\
+                typedef char yes;\
+                typedef struct { char _[2]; } no;\
+                template <class C> static yes _test(decltype(&C::contains));\
                 template <class C> static yes _test(decltype(&C::template contains<V>));
-                template <class C> static yes _test(decltype(&C::contains));
-                template <class C> static no _test(...);
-                static const bool value = sizeof( _test<T>(nullptr)) == sizeof(yes);
+                template <class C> static no _test(...);\
+                static const bool value = sizeof( _test<typename std::remove_reference<T>::type>(nullptr)) == sizeof(yes);\
             };
-        /* trait to check if the type has a shape member { */
 
-        template<typename T>
-            struct has_shape
-            {
-                typedef char yes;
-                typedef struct { char _[2]; } no;
+        /* trait to check if the type has a shape member */
+        MEMBER_ATTR_TRAIT(has_shape, shape);
 
-                template <class C> static yes _test(decltype(std::declval<C>().shape)*);
-                template <class C> static no _test(...);
-                static const bool value = sizeof( _test<T>(nullptr)) == sizeof(yes);
-            };
-        /* } */
-
-        /* trait to check if the type has a static size { */
+        /* trait to check if the type has a static size */
         template<class T>
             struct len_of {
                 static long constexpr value = -1;
             };
-        /* } */
-
     }
-
 }
 
 #endif

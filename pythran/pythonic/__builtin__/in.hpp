87abdb73d8a9faf17835ca06a20788f546574927
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2015-02-20 22:37:15 +0100

    Do not wrap string literals in a types::str whenever possible
    * only "string"[expr] and "string" op expr are wrapped
    * type inference is slightly more complex now as two types can be used:
            - const char (&)[N]
            - types::str
    * impressive performance speedup when doing dummy string manipulations!

--- pythran/pythonic/__builtin__/in.hpp
+++ pythran/pythonic/__builtin__/in.hpp
@@ -6,22 +6,27 @@
 
 namespace pythonic {
 
-    template<class T, class V, bool F>
-        struct _in {
-            bool operator()(T t, V const& v) const {
+    template<bool has_contains> struct _in;
+    template<>
+        struct _in<false>{
+          template<class T, class V>
+            bool operator()(T&& t, V const& v) const {
                 return std::find(t.begin(), t.end(), v) != t.end();
             }
         };
-    template<class T, class V>
-        struct _in<T, V, true> {
-            bool operator()(T t, V const& v) const {
+    template<>
+        struct _in<true> {
+          template<class T, class V>
+            bool operator()(T&& t, V const& v) const {
                 return t.contains(v);
             }
         };
 
     template <class T, class V>
         bool in(T &&t, V const &v) {
-            return _in<T, V, types::has_contains<typename std::remove_cv<typename std::remove_reference<T>::type>::type, V>::value>()(std::forward<T>(t), v);
+            using RT = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+            static bool constexpr has_contains = types::has_contains<RT, V>::value;
+            return _in<has_contains>()(std::forward<T>(t), v);
         }
 
 }

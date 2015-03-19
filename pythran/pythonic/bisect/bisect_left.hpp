9fa899721ea02001ff28fac05bef4d8f1228589b
Pierrick Brunet <pierrick.brunet@inria.fr>
2015-03-19 09:31:59 +0100

    Split header between declaration and implementation

--- pythran/pythonic/bisect/bisect_left.hpp
+++ pythran/pythonic/bisect/bisect_left.hpp
@@ -2,6 +2,7 @@
 #define PYTHONIC_BISECT_BISECTLEFT_HPP
 
 #include "pythonic/utils/proxy.hpp"
+#include "pythonic/include/bisect/bisect_left.hpp"
 
 #include <algorithm>
 
@@ -9,16 +10,15 @@ namespace pythonic {
 
     namespace bisect {
         template <class X, class A>
-            size_t bisect_left(X const& x, A const& a, long lo=0, long hi=-1) {
+            size_t bisect_left(X const& x, A const& a, long lo, long hi) {
                 if(hi<0) hi = x.end() - x.begin();
                 return std::lower_bound(x.begin()+lo, x.begin()+hi, a) - x.begin();
             }
 
-        PROXY(pythonic::bisect, bisect_left);
+        PROXY_IMPL(pythonic::bisect, bisect_left);
 
     }
 
 }
 
 #endif
-

a618ce2e59fcfd4d625b700057fd84bf0644b431
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-04-30 10:34:06 +0200

    Split decl.impl for finfo

---
+++ pythran/pythonic/include/types/finfo.hpp
@@ -0,0 +1,43 @@
+#ifndef PYTHONIC_INCLUDE_TYPES_FINFO_HPP
+#define PYTHONIC_INCLUDE_TYPES_FINFO_HPP
+
+#include "pythonic/types/attr.hpp"
+
+#include <limits>
+
+namespace pythonic {
+
+    namespace types {
+        template<class T>
+            struct finfo
+            {
+                T eps() const;
+            };
+
+    }
+}
+
+/* pythran attribute system { */
+namespace pythonic {
+    namespace types {
+        namespace __finfo {
+
+            template<int I, class T>
+                struct getattr;
+
+            template<class T>
+                struct getattr<attr::EPS,T>
+                {
+                    auto operator()(finfo<T> const& f)
+                    -> decltype(f.eps()) const;
+                };
+        }
+    }
+    namespace __builtin__ {
+        template<int I, class T>
+            auto getattr(pythonic::types::finfo<T> const& f)
+            -> decltype(pythonic::types::__finfo::getattr<I,T>()(f));
+    }
+}
+/* } */
+#endif
--- pythran/pythonic/types/finfo.hpp
+++ pythran/pythonic/types/finfo.hpp
@@ -1,6 +1,8 @@
 #ifndef PYTHONIC_TYPES_FINFO_HPP
 #define PYTHONIC_TYPES_FINFO_HPP
 
+#include "pythonic/include/types/finfo.hpp"
+
 #include "pythonic/types/attr.hpp"
 
 #include <limits>
@@ -9,32 +11,31 @@ namespace pythonic {
 
     namespace types {
         template<class T>
-            struct finfo {
-                T eps() const {
-                    return std::numeric_limits<T>::epsilon();
-                }
-            };
-
+            T finfo<T>::eps() const
+            {
+                return std::numeric_limits<T>::epsilon();
+            }
     }
 }
+
 /* pythran attribute system { */
 namespace pythonic {
     namespace types {
         namespace __finfo {
 
-            template<int I, class T>
-                struct getattr;
             template<class T>
-                struct getattr<attr::EPS,T> {
-                    auto operator()(finfo<T> const& f) -> decltype(f.eps()) const {
-                        return f.eps();
-                    }
-                };
+                auto getattr<attr::EPS,T>::operator()(finfo<T> const& f)
+                -> decltype(f.eps()) const
+                {
+                    return f.eps();
+                }
         }
     }
     namespace __builtin__ {
         template<int I, class T>
-            auto getattr(pythonic::types::finfo<T> const& f) -> decltype(pythonic::types::__finfo::getattr<I,T>()(f)) {
+            auto getattr(pythonic::types::finfo<T> const& f)
+            -> decltype(pythonic::types::__finfo::getattr<I,T>()(f))
+            {
                 return pythonic::types::__finfo::getattr<I,T>()(f);
             }
     }

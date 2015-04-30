fd07d1e1a334ac18750403d4ef6606b75eb477c4
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-04-30 10:35:07 +0200

    Split decl.impl for generator

--- pythran/pythonic/types/generator.hpp
+++ pythran/pythonic/types/generator.hpp
@@ -1,6 +1,8 @@
 #ifndef PYTHONIC_TYPES_GENERATOR_HPP
 #define PYTHONIC_TYPES_GENERATOR_HPP
 
+#include "pythonic/include/types/generator.hpp"
+
 #include "pythonic/__builtin__/StopIteration.hpp"
 #include <iterator>
 
@@ -8,36 +10,50 @@ namespace pythonic {
 
     namespace types {
         template <class T>
-            struct generator_iterator : std::iterator<std::forward_iterator_tag, typename T::result_type, ptrdiff_t, typename T::result_type*, typename T::result_type /* no ref */>
-        {
+            generator_iterator<T>::generator_iterator() :
+                the_generator()
+            {
+                the_generator.__generator_state = -1;
+            } // this represents the end
 
-            T the_generator;
-            generator_iterator() : the_generator() { the_generator.__generator_state = -1 ;} // this represents the end
-            generator_iterator(T const& a_generator) : the_generator(a_generator) {
-            }
-            generator_iterator& operator++() {
-                try { the_generator.next(); }
-                catch(types::StopIteration const&) {
-                    the_generator.__generator_state = -1;
+        template <class T>
+            generator_iterator<T>::generator_iterator(T const& a_generator) :
+                the_generator(a_generator)
+            {}
+
+        template <class T>
+            generator_iterator<T>& generator_iterator<T>::operator++()
+            {
+                try {
+                    the_generator.next();
+                } catch(types::StopIteration const&) {
+                        the_generator.__generator_state = -1;
                 }
                 return *this;
             }
-            typename T::result_type operator*() const {
+
+        template <class T>
+            typename T::result_type generator_iterator<T>::operator*() const
+            {
                 return *the_generator;
             }
-            bool operator!=(generator_iterator<T> const & other) const {
+
+        template <class T>
+            bool generator_iterator<T>::operator!=(generator_iterator<T> const & other) const
+            {
                 assert( other.the_generator.__generator_state == -1);
                 return the_generator.__generator_state != other.the_generator.__generator_state ;
             }
-            bool operator<(generator_iterator<T> const & other) const {
+
+        template <class T>
+            bool generator_iterator<T>::operator<(generator_iterator<T> const & other) const 
+            {
                 assert( other.the_generator.__generator_state == -1);
                 return the_generator.__generator_state != other.the_generator.__generator_state ;
             }
-        };
 
     }
 
 }
 
 #endif
-

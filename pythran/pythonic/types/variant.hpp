a3485c84dd0d7261410ade52695f37f71450511d
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-04-30 10:34:18 +0200

    Split decl.impl for variant

--- pythran/pythonic/types/variant.hpp
+++ pythran/pythonic/types/variant.hpp
@@ -1,63 +1,88 @@
 #ifndef PYTHONIC_TYPES_VARIANT_HPP
 #define PYTHONIC_TYPES_VARIANT_HPP
 
+#include "pythonic/include/types/variant.hpp"
+
 #include <utility>
 
 namespace pythonic {
 
     namespace types {
-        template <class... Types>
-            struct variant {};
 
         template <class Type>
-            struct variant<Type> {
-                char mem[sizeof(Type)];
-                Type* t;
+            variant<Type>::variant() :
+                t(nullptr)
+            {}
 
-                variant() : t(nullptr) {}
-                variant(Type const& t) : t(new (mem) Type(t)) {}
-                variant(variant<Type> const& t) : t(t.t?new (mem) Type(*t.t):t.t) {}
-                template<class OtherType>
-                    variant(variant<OtherType> const& t) : t(nullptr) {}
+        template <class Type>
+            variant<Type>::variant(Type const& t) :
+                t(new (mem) Type(t))
+            {}
 
-                template <class... Args>
-                    auto operator()(Args&&... args) -> decltype( std::declval<Type>()(std::forward<Args>(args)...) ) {
-                        return (*t)(std::forward<Args>(args)...);
-                    }
+        template <class Type>
+            variant<Type>::variant(variant<Type> const& t) :
+                t(t.t?new (mem) Type(*t.t):t.t)
+            {}
 
-            };
+        template <class Type>
+        template<class OtherType>
+            variant<Type>::variant(variant<OtherType> const& t) :
+                t(nullptr)
+            {}
+
+        template <class Type>
+        template <class... Args>
+            auto variant<Type>::operator()(Args&&... args) ->
+            decltype( std::declval<Type>()(std::forward<Args>(args)...) )
+            {
+                return (*t)(std::forward<Args>(args)...);
+            }
 
         template<class Type, class... Types>
-            struct variant<Type, Types...> {
-                typedef void callable;
-                char mem[sizeof(Type)];
-                Type* t;
-                variant<Types...> ot;
+            variant<Type, Types...>::variant() :
+                t(nullptr),
+                ot()
+            {}
 
-                variant() : t(nullptr), ot() {}
-                variant(Type const& t) : t(new (mem) Type(t)), ot() {}
-                template<class T>
-                    variant(T const& t) : t(nullptr), ot(t) {}
+        template<class Type, class... Types>
+            variant<Type, Types...>::variant(Type const& t) :
+                t(new (mem) Type(t)),
+                ot()
+            {}
 
-                // the below constructor handle case when Types and OtherTypes are not exactly the same
-                // or are in a different order
-                template<class OtherType, class... OtherTypes>
-                    variant(variant<OtherType, OtherTypes...> const& t) :
-                        t(nullptr),
-                        ot(t.ot) {}
+        template<class Type, class... Types>
+        template<class T>
+            variant<Type, Types...>::variant(T const& t) :
+                t(nullptr),
+                ot(t)
+            {}
 
-                template<class... OtherTypes>
-                    variant(variant<Type, OtherTypes...> const& t) :
-                        t(t.t?new (mem) Type(*t.t):t.t),
-                        ot(t.ot) {}
+        // the below constructor handle case when Types and OtherTypes are not exactly the same
+        // or are in a different order
+        template<class Type, class... Types>
+        template<class OtherType, class... OtherTypes>
+            variant<Type, Types...>::variant(variant<OtherType, OtherTypes...> const& t) :
+                t(nullptr),
+                ot(t.ot)
+            {}
 
-                template <class... Args>
-                    auto operator()(Args&&... args) -> decltype( std::declval<Type>()(args...)){
-                        if(t) return (*t)(args...);
-                        else return ot(args...);
-                    }
-            };
+        template<class Type, class... Types>
+        template<class... OtherTypes>
+            variant<Type, Types...>::variant(variant<Type, OtherTypes...> const& t) :
+                t(t.t?new (mem) Type(*t.t):t.t),
+                ot(t.ot)
+            {}
 
+        template<class Type, class... Types>
+        template <class... Args>
+            auto variant<Type, Types...>::operator()(Args&&... args)
+            -> decltype( std::declval<Type>()(args...))
+            {
+                if(t)
+                    return (*t)(args...);
+                else
+                    return ot(args...);
+            }
     }
 }
 #endif

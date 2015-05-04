fc96930bdac0c4a2e248483b4c9c0e221ee34f44
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-05-04 09:13:14 +0200

    Split decl/impl for nditerator

---
+++ pythran/pythonic/include/types/nditerator.hpp
@@ -0,0 +1,96 @@
+#ifndef PYTHONIC_INCLUDE_TYPES_NDITERATOR_HPP
+#define PYTHONIC_INCLUDE_TYPES_NDITERATOR_HPP
+
+#include <iterator>
+
+namespace pythonic {
+
+    namespace types {
+        /* Iterator over whatever provides a fast(long) method to access its element
+         */
+        template<class E>
+            struct nditerator : public std::iterator<std::random_access_iterator_tag,
+                                                     typename std::remove_reference<decltype(std::declval<E&>().fast(0))>::type,
+                                                     ptrdiff_t,
+                                                     typename std::remove_reference<decltype(std::declval<E&>().fast(0))>::type*,
+                                                     typename std::remove_reference<decltype(std::declval<E&>().fast(0))>::type /* no ref here, becasue we return a proxy object */
+                                                     >
+            {
+                E & data;
+                long index;
+                nditerator(E & data, long index);
+
+                auto operator*() -> decltype(data.fast(index));
+                auto operator*() const -> decltype(data.fast(index));
+                nditerator<E>& operator++();
+                nditerator<E>& operator--();
+                nditerator<E>& operator+=(long i);
+                nditerator<E>& operator-=(long i);
+                nditerator<E> operator+(long i) const;
+                nditerator<E> operator-(long i) const;
+                long operator-(nditerator<E> const& other) const;
+                bool operator!=(nditerator<E> const& other) const;
+                bool operator==(nditerator<E> const& other) const;
+                bool operator<(nditerator<E> const& other) const;
+                nditerator& operator=(nditerator const& other);
+            };
+
+        /* Const iterator over whatever provides a fast(long) method to access its element
+         */
+        template<class E>
+            struct const_nditerator : public std::iterator<std::random_access_iterator_tag,
+                                                           typename std::remove_reference<decltype(std::declval<E&>().fast(0))>::type>
+            {
+                E const &data;
+                long index;
+                const_nditerator(E const& data, long index);
+
+                auto operator*() const -> decltype(data.fast(index));
+                const_nditerator<E>& operator++();
+                const_nditerator<E>& operator--();
+                const_nditerator<E>& operator+=(long i);
+                const_nditerator<E>& operator-=(long i);
+                const_nditerator<E> operator+(long i) const;
+                const_nditerator<E> operator-(long i) const;
+                long operator-(const_nditerator<E> const& other) const;
+                bool operator!=(const_nditerator<E> const& other) const;
+                bool operator==(const_nditerator<E> const& other) const;
+                bool operator<(const_nditerator<E> const& other) const;
+                const_nditerator& operator=(const_nditerator const& other);
+            };
+
+        // build an iterator over T, selecting a raw pointer if possible
+        template<bool is_strided>
+            struct make_nditerator
+            {
+                template<class T>
+                    auto operator()(T& self, long i) -> decltype(nditerator<T>(self, i)) const;
+            };
+
+        template<>
+            struct make_nditerator<false>
+            {
+                template<class T>
+                    typename T::dtype * operator()(T& self, long i) const;
+            };
+
+        template<bool is_strided>
+            struct make_const_nditerator
+            {
+                template<class T>
+                    auto operator()(T const& self, long i)
+                    -> decltype(const_nditerator<T>(self, i)) const;
+            };
+
+        template<>
+            struct make_const_nditerator<false>
+            {
+                template<class T>
+                    typename T::dtype const* operator()(T const& self, long i) const;
+            };
+
+    }
+
+}
+
+#endif
--- pythran/pythonic/types/nditerator.hpp
+++ pythran/pythonic/types/nditerator.hpp
@@ -1,6 +1,8 @@
 #ifndef PYTHONIC_TYPES_NDITERATOR_HPP
 #define PYTHONIC_TYPES_NDITERATOR_HPP
 
+#include "pythonic/include/types/nditerator.hpp"
+
 #include <iterator>
 
 namespace pythonic {
@@ -9,104 +11,217 @@ namespace pythonic {
         /* Iterator over whatever provides a fast(long) method to access its element
          */
         template<class E>
-            struct nditerator : public std::iterator<std::random_access_iterator_tag,
-                                                     typename std::remove_reference<decltype(std::declval<E&>().fast(0))>::type,
-                                                     ptrdiff_t,
-                                                     typename std::remove_reference<decltype(std::declval<E&>().fast(0))>::type*,
-                                                     typename std::remove_reference<decltype(std::declval<E&>().fast(0))>::type /* no ref here, becasue we return a proxy object */
-                                                     >
-            {
-                E & data;
-                long index;
-                nditerator(E & data, long index) : data(data), index(index) {}
+            nditerator<E>::nditerator(E & data, long index) :
+                data(data),
+                index(index)
+            {}
 
-                auto operator*() -> decltype(data.fast(index)) { return data.fast(index); }
-                auto operator*() const -> decltype(data.fast(index)) { return data.fast(index); }
-                nditerator<E>& operator++() { ++index; return *this;}
-                nditerator<E>& operator--() { --index; return *this;}
-                nditerator<E>& operator+=(long i) { index += i; return *this;}
-                nditerator<E>& operator-=(long i) { index -= i; return *this;}
-                nditerator<E> operator+(long i) const { nditerator<E> other(*this); other += i; return other; }
-                nditerator<E> operator-(long i) const { nditerator<E> other(*this); other -= i; return other; }
-                long operator-(nditerator<E> const& other) const { return index - other.index; }
-                bool operator!=(nditerator<E> const& other) const {
-                    return index != other.index;
-                }
-                bool operator==(nditerator<E> const& other) const {
-                    return index == other.index;
-                }
-                bool operator<(nditerator<E> const& other) const {
-                    return index < other.index;
-                }
-                nditerator& operator=(nditerator const& other) {
-                  assert(&data == &other.data);
-                  index = other.index;
-                  return *this;
-                }
-            };
+        template<class E>
+            auto nditerator<E>::operator*() -> decltype(data.fast(index))
+            {
+                return data.fast(index);
+            }
+
+        template<class E>
+            auto nditerator<E>::operator*() const -> decltype(data.fast(index))
+            {
+                return data.fast(index);
+            }
+
+        template<class E>
+            nditerator<E>& nditerator<E>::operator++()
+            {
+                ++index;
+                return *this;
+            }
+
+        template<class E>
+            nditerator<E>& nditerator<E>::operator--()
+            {
+                --index;
+                return *this;
+            }
+
+        template<class E>
+            nditerator<E>& nditerator<E>::operator+=(long i)
+            {
+                index += i;
+                return *this;
+            }
+
+        template<class E>
+            nditerator<E>& nditerator<E>::operator-=(long i)
+            {
+                index -= i;
+                return *this;
+            }
+
+        template<class E>
+            nditerator<E> nditerator<E>::operator+(long i) const
+            {
+                nditerator<E> other(*this);
+                other += i;
+                return other;
+            }
+
+        template<class E>
+            nditerator<E> nditerator<E>::operator-(long i) const
+            {
+                nditerator<E> other(*this);
+                other -= i;
+                return other;
+            }
+
+        template<class E>
+            long nditerator<E>::operator-(nditerator<E> const& other) const
+            {
+                return index - other.index;
+            }
+
+        template<class E>
+            bool nditerator<E>::operator!=(nditerator<E> const& other) const
+            {
+                return index != other.index;
+            }
+
+        template<class E>
+            bool nditerator<E>::operator==(nditerator<E> const& other) const
+            {
+                return index == other.index;
+            }
+
+        template<class E>
+            bool nditerator<E>::operator<(nditerator<E> const& other) const
+            {
+                return index < other.index;
+            }
+
+        template<class E>
+            nditerator<E>& nditerator<E>::operator=(nditerator<E> const& other)
+            {
+              assert(&data == &other.data);
+              index = other.index;
+              return *this;
+            }
 
         /* Const iterator over whatever provides a fast(long) method to access its element
          */
         template<class E>
-            struct const_nditerator : public std::iterator<std::random_access_iterator_tag,
-                                                           typename std::remove_reference<decltype(std::declval<E&>().fast(0))>::type>
-            {
-                E const &data;
-                long index;
-                const_nditerator(E const& data, long index) : data(data), index(index) {
-                }
+            const_nditerator<E>::const_nditerator(E const& data, long index) :
+                data(data),
+                index(index)
+            {}
 
-                auto operator*() const -> decltype(data.fast(index)) { return data.fast(index); }
-                const_nditerator<E>& operator++() { ++index; return *this;}
-                const_nditerator<E>& operator--() { --index; return *this;}
-                const_nditerator<E>& operator+=(long i) { index +=  i; return *this;}
-                const_nditerator<E>& operator-=(long i) { index -=  i; return *this;}
-                const_nditerator<E> operator+(long i) const { const_nditerator<E> other(*this); other += i; return other; }
-                const_nditerator<E> operator-(long i) const { const_nditerator<E> other(*this); other -= i; return other; }
-                long operator-(const_nditerator<E> const& other) const {
-                    return index - other.index;
-                }
-                bool operator!=(const_nditerator<E> const& other) const {
-                    return index != other.index;
-                }
-                bool operator==(const_nditerator<E> const& other) const {
-                    return index == other.index;
-                }
-                bool operator<(const_nditerator<E> const& other) const {
-                    return  index <  other.index;
-                }
-                const_nditerator& operator=(const_nditerator const& other) { index = other.index; return *this;}
-            };
+        template<class E>
+            auto const_nditerator<E>::operator*() const
+            -> decltype(data.fast(index))
+            {
+                return data.fast(index);
+            }
+
+        template<class E>
+            const_nditerator<E>& const_nditerator<E>::operator++()
+            {
+                ++index;
+                return *this;
+            }
+
+        template<class E>
+            const_nditerator<E>& const_nditerator<E>::operator--()
+            {
+                --index;
+                return *this;
+            }
+
+        template<class E>
+            const_nditerator<E>& const_nditerator<E>::operator+=(long i)
+            {
+                index +=  i;
+                return *this;
+            }
+
+        template<class E>
+            const_nditerator<E>& const_nditerator<E>::operator-=(long i)
+            {
+                index -=  i;
+                return *this;
+            }
+
+        template<class E>
+            const_nditerator<E> const_nditerator<E>::operator+(long i) const
+            {
+                const_nditerator<E> other(*this);
+                other += i;
+                return other;
+            }
+
+        template<class E>
+            const_nditerator<E> const_nditerator<E>::operator-(long i) const
+            {
+                const_nditerator<E> other(*this);
+                other -= i;
+                return other;
+            }
+
+        template<class E>
+            long const_nditerator<E>::operator-(const_nditerator<E> const& other) const
+            {
+                return index - other.index;
+            }
+
+        template<class E>
+            bool const_nditerator<E>::operator!=(const_nditerator<E> const& other) const
+            {
+                return index != other.index;
+            }
+
+        template<class E>
+            bool const_nditerator<E>::operator==(const_nditerator<E> const& other) const
+            {
+                return index == other.index;
+            }
+
+        template<class E>
+            bool const_nditerator<E>::operator<(const_nditerator<E> const& other) const
+            {
+                return  index < other.index;
+            }
+
+        template<class E>
+            const_nditerator<E>& const_nditerator<E>::operator=(const_nditerator const& other)
+            {
+                index = other.index;
+                return *this;
+            }
 
         // build an iterator over T, selecting a raw pointer if possible
         template<bool is_strided>
-          struct make_nditerator {
-            template<class T>
-              auto operator()(T& self, long i) -> decltype(nditerator<T>(self, i)) const {
+        template<class T>
+            auto make_nditerator<is_strided>::operator()(T& self, long i)
+            -> decltype(nditerator<T>(self, i)) const
+            {
                 return nditerator<T>(self, i);
-              }
-          };
-        template<>
-          struct make_nditerator<false> {
-            template<class T>
-              typename T::dtype * operator()(T& self, long i) const {
+            }
+
+        template<class T>
+            typename T::dtype * make_nditerator<false>::operator()(T& self, long i) const
+            {
                 return self.buffer + i;
-              }
-          };
+            }
+
         template<bool is_strided>
-          struct make_const_nditerator {
-            template<class T>
-              auto operator()(T const& self, long i) -> decltype(const_nditerator<T>(self, i)) const {
+        template<class T>
+            auto make_const_nditerator<is_strided>::operator()(T const& self, long i)
+            -> decltype(const_nditerator<T>(self, i)) const
+            {
                 return const_nditerator<T>(self, i);
-              }
-          };
-        template<>
-          struct make_const_nditerator<false> {
-            template<class T>
-              typename T::dtype const* operator()(T const& self, long i) const {
+            }
+
+        template<class T>
+            typename T::dtype const* make_const_nditerator<false>::operator()(T const& self, long i) const
+            {
                 return self.buffer + i;
-              }
-          };
+            }
 
     }
 

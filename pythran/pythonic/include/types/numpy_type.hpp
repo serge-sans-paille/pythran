c41daf6c7233062e817b099a33add3cb26a8672e
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-05-05 16:10:39 +0200

    Split decl.impl for numpy_type

---
+++ pythran/pythonic/include/types/numpy_type.hpp
@@ -0,0 +1,36 @@
+#ifndef PYTHONIC_INCLUDE_TYPES_NUMPYTYPE_HPP
+#define PYTHONIC_INCLUDE_TYPES_NUMPYTYPE_HPP
+
+#include "pythonic/utils/proxy.hpp"
+
+namespace pythonic {
+
+    namespace types {
+
+        template<class T, typename EnableDefault = void>
+            struct numpy_type
+            {
+                using type = decltype(std::declval<T>()());
+            };
+
+        template<class T>
+            struct numpy_type<T, typename std::enable_if<std::is_fundamental<T>::value>::type>
+            {
+                using type = T;
+            };
+
+#define SPECIALIZE_NUMPY_TYPE(TYPE)                                             \
+        template<>                                                              \
+            struct numpy_type<TYPE>                                             \
+            {                                                                   \
+                using type = TYPE;                                              \
+            }
+
+        SPECIALIZE_NUMPY_TYPE(std::complex<float>);
+        SPECIALIZE_NUMPY_TYPE(std::complex<double>);
+#undef SPECIALIZE_NUMPY_TYPE
+    }
+
+}
+
+#endif
--- pythran/pythonic/types/numpy_type.hpp
+++ pythran/pythonic/types/numpy_type.hpp
@@ -1,29 +1,6 @@
 #ifndef PYTHONIC_TYPES_NUMPYTYPE_HPP
 #define PYTHONIC_TYPES_NUMPYTYPE_HPP
 
-#include "pythonic/utils/proxy.hpp"
-
-namespace pythonic {
-
-    namespace types {
-
-        template<class T, typename EnableDefault = void>
-            struct numpy_type {
-                typedef decltype(std::declval<T>()()) type;
-            };
-
-        template<class T>
-            struct numpy_type<T, typename std::enable_if<std::is_fundamental<T>::value>::type> {
-               typedef T type;
-            };
-
-#define SPECIALIZE_NUMPY_TYPE(TYPE) template<> struct numpy_type<TYPE> { typedef TYPE type; }
-        SPECIALIZE_NUMPY_TYPE(std::complex<float>);
-        SPECIALIZE_NUMPY_TYPE(std::complex<double>);
-#undef SPECIALIZE_NUMPY_TYPE
-    }
-
-}
+#include "pythonic/include/types/numpy_type.hpp"
 
 #endif
-

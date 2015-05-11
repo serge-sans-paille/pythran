35aa733bb4a1b88acc2e18439d4674ef1c4ce1da
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-05-11 15:30:04 +0200

    Split decl/impl for numpy.dot

--- pythran/pythonic/include/numpy/dot.hpp
+++ pythran/pythonic/include/numpy/dot.hpp
@@ -1,7 +1,5 @@
-#ifndef PYTHONIC_NUMPY_DOT_HPP
-#define PYTHONIC_NUMPY_DOT_HPP
-
-#include "pythonic/include/numpy/dot.hpp"
+#ifndef PYTHONIC_INCLUDE_NUMPY_DOT_HPP
+#define PYTHONIC_INCLUDE_NUMPY_DOT_HPP
 
 #include "pythonic/types/ndarray.hpp"
 #include "pythonic/numpy/sum.hpp"
@@ -10,6 +8,18 @@
 
 #include <nt2/linalg/details/blas/mm.hpp>
 #include <nt2/linalg/details/blas/mv.hpp>
+
+template <class T>
+    struct is_blas_type : pythonic::types::is_complex<T>
+    {};
+
+template<>
+    struct is_blas_type<float> : std::integral_constant<bool, true>
+    {};
+
+template<>
+    struct is_blas_type<double> : std::integral_constant<bool, true>
+    {};
 
 
 namespace pythonic {
@@ -20,20 +30,14 @@ namespace pythonic {
                                     and (std::is_scalar<F>::value or types::is_complex<F>::value),
                                     decltype(std::declval<E>()*std::declval<F>())
                                     >::type
-                dot(E const& e, F const& f)
-                {
-                    return e*f;
-                }
+                dot(E const& e, F const& f);
 
         template<class E, class F>
             typename std::enable_if<types::is_numexpr_arg<E>::value and types::is_numexpr_arg<F>::value // Arguments are array_like
                                     and E::value == 1 and F::value ==1, // It is a two vectors.
                                     typename __combined<typename E::dtype, typename F::dtype>::type
                                    >::type
-                dot(E const& e, F const& f)
-                {
-                    return sum(types::numpy_expr<operator_::proxy::mul, E, F>(e, f));
-                }
+                dot(E const& e, F const& f);
 
         /// Matrice / Vector multiplication
 
@@ -42,28 +46,14 @@ namespace pythonic {
             typename std::enable_if<is_blas_type<E>::value,
                                     types::ndarray<E, 1>
                                     >::type
-            dot(types::ndarray<E, 2> const& f, types::ndarray<E, 1> const& e)
-            {
-                types::ndarray<E, 1> out(types::array<long, 1>{{f.shape[0]}}, __builtin__::None);
-                E al = 1, be = 0;
-                const int m = f.shape[1], n = f.shape[0], inc = 1;
-                nt2::details::gemv("T", &m, &n, &al, f.buffer, &m, e.buffer, &inc, &be, out.buffer, &inc);
-                return out;
-            }
+            dot(types::ndarray<E, 2> const& f, types::ndarray<E, 1> const& e);
 
         // The trick is to not transpose the matrix so that MV become VM
         template<class E>
             typename std::enable_if<is_blas_type<E>::value,
                                     types::ndarray<E, 1>
                                     >::type
-            dot(types::ndarray<E, 1> const& e, types::ndarray<E, 2> const& f)
-            {
-                types::ndarray<E, 1> out(types::array<long, 1>{{f.shape[1]}}, __builtin__::None);
-                E al = 1, be = 0;
-                const int m = f.shape[1], n = f.shape[0], inc = 1;
-                nt2::details::gemv("N", &m, &n, &al, f.buffer, &m, e.buffer, &inc, &be, out.buffer, &inc);
-                return out;
-            }
+            dot(types::ndarray<E, 1> const& e, types::ndarray<E, 2> const& f);
 
         // If arguments could be use with blas, we evaluate them as we need pointer on array for blas
         template<class E, class F>
@@ -73,11 +63,7 @@ namespace pythonic {
                                     and E::value == 2 and F::value == 1, // And it is matrix / vect
                                     types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 1>
                                    >::type
-                dot(E const& e, F const& f) {
-                    types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 2> e_ = e;
-                    types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 1> f_ = f;
-                    return dot(e_, f_);
-                }
+                dot(E const& e, F const& f);
 
         // If arguments could be use with blas, we evaluate them as we need pointer on array for blas
         template<class E, class F>
@@ -87,11 +73,7 @@ namespace pythonic {
                                     and E::value == 1 and F::value == 2, // And it is vect / matrix
                                     types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 1>
                                    >::type
-                dot(E const& e, F const& f) {
-                    types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 1> e_ = e;
-                    types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 2> f_ = f;
-                    return dot(e_, f_);
-                }
+                dot(E const& e, F const& f);
 
         // If one of the arg doesn't have a "blas compatible type", we use a slow matrix vector multiplication.
         template<class E, class F>
@@ -99,14 +81,7 @@ namespace pythonic {
                                     and E::value == 1 and F::value == 2, // And it is vect / matrix
                                     types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 1>
                                     >::type
-            dot(E const& e, F const& f)
-            {
-                types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 1> out(types::array<long, 1>{{f.shape[1]}}, 0);
-                for(long i=0; i<out.shape[0]; i++)
-                    for(long j=0; j<f.shape[0]; j++)
-                        out[i] += e[j] * f[types::array<long, 2>{{j, i}}];
-                return out;
-            }
+            dot(E const& e, F const& f);
 
         // If one of the arg doesn't have a "blas compatible type", we use a slow matrix vector multiplication.
         template<class E, class F>
@@ -114,14 +89,7 @@ namespace pythonic {
                                     and E::value == 2 and F::value == 1, // And it is vect / matrix
                                     types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 1>
                                     >::type
-            dot(E const& e, F const& f)
-            {
-                types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 1> out(types::array<long, 1>{{e.shape[0]}}, 0);
-                for(long i=0; i<out.shape[0]; i++)
-                    for(long j=0; j<f.shape[0]; j++)
-                        out[i] += e[types::array<long, 2>{{i, j}}] * f[j];
-                return out;
-            }
+            dot(E const& e, F const& f);
 
         /// Matrix / Matrix multiplication
 
@@ -133,14 +101,7 @@ namespace pythonic {
             typename std::enable_if<is_blas_type<E>::value,
                                     types::ndarray<E, 2>
                                     >::type
-            dot(types::ndarray<E, 2> const& a, types::ndarray<E, 2> const& b)
-            {
-                E al = 1, be = 0;
-                int m = b.shape[1], n = a.shape[0], k = b.shape[0];
-                types::ndarray<E, 2> out(types::array<long, 2>{{m, n}}, __builtin__::None);
-                nt2::details::gemm("N", "N", &m, &n, &k, &al, b.buffer, &m, a.buffer, &k, &be, out.buffer, &m);
-                return out;
-            }
+            dot(types::ndarray<E, 2> const& a, types::ndarray<E, 2> const& b);
 
         // If arguments could be sue with blas, we evaluate them as we need pointer on array for blas
         template<class E, class F>
@@ -150,11 +111,7 @@ namespace pythonic {
                                     and E::value == 2 and F::value == 2, // And both are matrix
                                     types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 2>
                                    >::type
-                dot(E const& e, F const& f) {
-                    types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 2> e_ = e;
-                    types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 2> f_ = f;
-                    return dot(e_, f_);
-                }
+                dot(E const& e, F const& f);
 
         // If one of the arg doesn't have a "blas compatible type", we use a slow matrix multiplication.
         template<class E, class F>
@@ -162,17 +119,9 @@ namespace pythonic {
                                     and E::value == 2 and F::value == 2, // And it is matrix / matrix
                                     types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 2>
                                     >::type
-            dot(E const& e, F const& f)
-            {
-                types::ndarray<typename __combined<typename E::dtype, typename F::dtype>::type, 2> out(types::array<long, 2>{{e.shape[0], f.shape[1]}}, 0);
-                for(long i=0; i<out.shape[0]; i++)
-                    for(long j=0; j<out.shape[1]; j++)
-                        for(long k=0; k<e.shape[1]; k++)
-                            out[types::array<long, 2>{{i, j}}] += e[types::array<long, 2>{{i, k}}] * f[types::array<long, 2>{{k, j}}];
-                return out;
-            }
+            dot(E const& e, F const& f);
 
-        PROXY_IMPL(pythonic::numpy, dot);
+        PROXY_DECL(pythonic::numpy, dot);
 
     }
 

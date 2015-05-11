c2c8769a06c0326e1893de587f0620100bb765c5
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-05-11 12:47:58 +0200

    Split decl/impl for type traits

---
+++ pythran/pythonic/include/types/traits.hpp
@@ -0,0 +1,90 @@
+#ifndef PYTHONIC_INCLUDE_TYPES_TRAITS_HPP
+#define PYTHONIC_INCLUDE_TYPES_TRAITS_HPP
+
+#include <complex>
+
+namespace pythonic {
+
+    namespace types {
+
+        /* could overload is_scalar to consider complex has scalar types */
+        template<class T>
+            struct is_complex
+            {
+                static constexpr bool value = false;
+                using type = T;
+            };
+
+        template<class T>
+            struct is_complex<std::complex<T>>
+            {
+                static constexpr bool value = true;
+                using type = T;
+            };
+
+#define MEMBER_TYPE_TRAIT(check_struct, member)                                 \
+        template<typename T>                                                    \
+            struct check_struct                                                 \
+            {                                                                   \
+                using yes = char;                                               \
+                using no = struct { char _[2]; };                               \
+                template <class C>                                              \
+                    static yes _test(typename C::member*);                      \
+                template <class C>                                              \
+                    static no _test(...);                                       \
+                static const bool value = sizeof( _test<typename std::remove_reference<T>::type>(nullptr)) == sizeof(yes);\
+            };
+
+#define MEMBER_ATTR_TRAIT(check_struct, member)                                 \
+        template<typename T>                                                    \
+            struct check_struct                                                 \
+            {                                                                   \
+                using yes = char;                                               \
+                using no = struct { char _[2]; };                               \
+                template <class C>                                              \
+                    static yes _test(decltype(&C::member));                     \
+                template <class C>                                              \
+                    static no _test(...);                                       \
+                static const bool value = sizeof( _test<typename std::remove_reference<T>::type>(nullptr)) == sizeof(yes);\
+            };
+
+        /* trait to check if a type is iterable*/
+        MEMBER_TYPE_TRAIT(is_iterable, iterator);
+
+        /* trait to check if a type is callable */
+        MEMBER_TYPE_TRAIT(is_callable, callable);
+
+        /* trait to check if a type is pure */
+        MEMBER_TYPE_TRAIT(is_pure, pure);
+
+        /* trait to check if the type has a size member */
+        MEMBER_ATTR_TRAIT(has_size, size);
+
+        /* trait to check if the type has a contains member */
+        template<typename T, class V>
+            struct has_contains
+            {
+                using yes = char;
+                using no = struct { char _[2]; };
+                template <class C>
+                    static yes _test(decltype(&C::contains));
+                template <class C>
+                    static yes _test(decltype(&C::template contains<V>));
+                template <class C>
+                    static no _test(...);
+                static const bool value = sizeof( _test<typename std::remove_reference<T>::type>(nullptr)) == sizeof(yes);
+            };
+
+        /* trait to check if the type has a shape member */
+        MEMBER_ATTR_TRAIT(has_shape, shape);
+
+        /* trait to check if the type has a static size */
+        template<class T>
+            struct len_of
+            {
+                static long constexpr value = -1;
+            };
+    }
+}
+
+#endif

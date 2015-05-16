ef2421dc1873f91cb1157fedbf8ea2fb248d5ca9
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-05-16 09:27:11 +0200

    if uncorrect NDEBUG flag

--- pythran/pythonic/__builtin__/assert.hpp
+++ pythran/pythonic/__builtin__/assert.hpp
@@ -1,16 +1,17 @@
 #ifndef PYTHONIC_BUILTIN_ASSERT_HPP
 #define PYTHONIC_BUILTIN_ASSERT_HPP
 
-#include "pythonic/types/str.hpp"
 #include "pythonic/include/__builtin__/assert.hpp"
-#include "pythonic/__builtin__/AssertionError.hpp"
+
+#include "pythonic/types/exceptions.hpp"
+#include "pythonic/types/str.hpp"
 
 namespace pythonic
 {
 
   void pythran_assert(bool cond, types::str const &what)
   {
-#ifdef NDEBUG
+#ifndef NDEBUG
     if (not cond)
       throw types::AssertionError(what);
 #endif

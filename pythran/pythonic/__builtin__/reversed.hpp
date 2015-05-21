b0647a78b412404228cefc97fefa4cb2a8552635
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-05-21 11:31:33 +0200

    mode reversed impl in details namespace

--- pythran/pythonic/__builtin__/reversed.hpp
+++ pythran/pythonic/__builtin__/reversed.hpp
@@ -1,56 +1,60 @@
 #ifndef PYTHONIC_BUILTIN_REVERSED_HPP
 #define PYTHONIC_BUILTIN_REVERSED_HPP
 
-#include "pythonic/utils/proxy.hpp"
 #include "pythonic/include/__builtin__/reversed.hpp"
+
+#include "pythonic/utils/proxy.hpp"
 
 namespace pythonic
 {
 
   namespace __builtin__
   {
-
-    template <class Iterable>
-    _reversed<Iterable>::_reversed()
+    namespace details
     {
+
+      template <class Iterable>
+      reversed<Iterable>::reversed()
+      {
+      }
+
+      template <class Iterable>
+      reversed<Iterable>::reversed(Iterable const &iterable)
+          : iterable(iterable)
+      {
+      }
+
+      template <class Iterable>
+      typename reversed<Iterable>::iterator reversed<Iterable>::begin()
+      {
+        return iterable.rbegin();
+      }
+
+      template <class Iterable>
+      typename reversed<Iterable>::iterator reversed<Iterable>::end()
+      {
+        return iterable.rend();
+      }
+
+      template <class Iterable>
+      typename reversed<Iterable>::const_iterator
+      reversed<Iterable>::begin() const
+      {
+        return iterable.rbegin();
+      }
+
+      template <class Iterable>
+      typename reversed<Iterable>::const_iterator
+      reversed<Iterable>::end() const
+      {
+        return iterable.rend();
+      }
     }
 
     template <class Iterable>
-    _reversed<Iterable>::_reversed(Iterable const &iterable)
-        : iterable(iterable)
+    details::reversed<Iterable> reversed(Iterable const &iterable)
     {
-    }
-
-    template <class Iterable>
-    typename _reversed<Iterable>::iterator _reversed<Iterable>::begin()
-    {
-      return iterable.rbegin();
-    }
-
-    template <class Iterable>
-    typename _reversed<Iterable>::iterator _reversed<Iterable>::end()
-    {
-      return iterable.rend();
-    }
-
-    template <class Iterable>
-    typename _reversed<Iterable>::const_iterator
-    _reversed<Iterable>::begin() const
-    {
-      return iterable.rbegin();
-    }
-
-    template <class Iterable>
-    typename _reversed<Iterable>::const_iterator
-    _reversed<Iterable>::end() const
-    {
-      return iterable.rend();
-    }
-
-    template <class Iterable>
-    _reversed<Iterable> reversed(Iterable const &iterable)
-    {
-      return _reversed<Iterable>(iterable);
+      return {iterable};
     }
 
     PROXY_IMPL(pythonic::__builtin__, reversed);
--- pythran/pythonic/include/__builtin__/reversed.hpp
+++ pythran/pythonic/include/__builtin__/reversed.hpp
@@ -9,25 +9,28 @@ namespace pythonic
   namespace __builtin__
   {
 
+    namespace details
+    {
+      template <class Iterable>
+      struct reversed {
+
+        using value_type = typename Iterable::value_type;
+        using iterator = typename Iterable::reverse_iterator;
+        using const_iterator = typename Iterable::const_reverse_iterator;
+
+        Iterable iterable;
+
+        reversed();
+        reversed(Iterable const &iterable);
+        iterator begin();
+        iterator end();
+        const_iterator begin() const;
+        const_iterator end() const;
+      };
+    }
+
     template <class Iterable>
-    struct _reversed {
-
-      using value_type = typename Iterable::value_type;
-      using iterator = typename Iterable::reverse_iterator;
-      using const_iterator = typename Iterable::const_reverse_iterator;
-
-      Iterable iterable;
-
-      _reversed();
-      _reversed(Iterable const &iterable);
-      iterator begin();
-      iterator end();
-      const_iterator begin() const;
-      const_iterator end() const;
-    };
-
-    template <class Iterable>
-    _reversed<Iterable> reversed(Iterable const &iterable);
+    details::reversed<Iterable> reversed(Iterable const &iterable);
 
     PROXY_DECL(pythonic::__builtin__, reversed);
   }

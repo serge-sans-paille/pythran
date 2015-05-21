32169e195be4511b09e0b94a14b217b7a755c37c
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-05-21 09:21:20 +0200

    __builtin__.map may return types::array instead of tuple

--- pythran/pythonic/include/__builtin__/map.hpp
+++ pythran/pythonic/include/__builtin__/map.hpp
@@ -1,9 +1,10 @@
 #ifndef PYTHONIC_INCLUDE_BUILTIN_MAP_HPP
 #define PYTHONIC_INCLUDE_BUILTIN_MAP_HPP
 
-#include "pythonic/utils/proxy.hpp"
-#include "pythonic/types/list.hpp"
-#include "pythonic/types/tuple.hpp"
+#include "pythonic/include/utils/proxy.hpp"
+#include "pythonic/include/types/list.hpp"
+#include "pythonic/include/types/none.hpp"
+#include "pythonic/include/types/tuple.hpp"
 
 #include <utility>
 
@@ -13,24 +14,27 @@ namespace pythonic
   namespace __builtin__
   {
 
-    template <typename Operator, typename List0, typename... Iterators>
-    auto _map(Operator &op, List0 &&seq, Iterators... iterators)
-        -> types::list<decltype(op(*seq.begin(), *iterators...))>;
+    namespace details
+    {
+      template <typename Operator, typename List0, typename... Iterators>
+      auto map(Operator &op, List0 &&seq, Iterators... iterators)
+          -> types::list<decltype(op(*seq.begin(), *iterators...))>;
 
-    template <typename List0, typename... Iterators>
-    types::list<std::tuple<
-        typename std::remove_reference<List0>::type::iterator::value_type,
-        typename Iterators::value_type...>>
-    _map(types::none_type, List0 &&seq, Iterators... iterators);
+      template <typename List0, typename... Iterators>
+      auto map(types::none_type, List0 &&seq, Iterators... iterators)
+          -> types::list<decltype(types::make_tuple(*seq.begin(),
+                                                    *iterators...))>;
 
-    template <typename List0>
-    types::list<
-        typename std::remove_reference<List0>::type::iterator::value_type>
-    _map(types::none_type, List0 &&seq);
+      template <typename List0>
+      types::list<
+          typename std::remove_reference<List0>::type::iterator::value_type>
+      map(types::none_type, List0 &&seq);
+    }
 
     template <typename Operator, typename List0, typename... ListN>
     auto map(Operator op, List0 &&seq, ListN &&... lists)
-        -> decltype(_map(op, std::forward<List0>(seq), lists.begin()...));
+        -> decltype(details::map(op, std::forward<List0>(seq),
+                                 lists.begin()...));
 
     PROXY_DECL(pythonic::__builtin__, map);
   }

ab4bdda52d2ed48fd166d0d474e81baa931e3f2a
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-05-21 16:21:39 +0200

    Move part of builtin.sum impl in details namespace

--- pythran/pythonic/__builtin__/sum.hpp
+++ pythran/pythonic/__builtin__/sum.hpp
@@ -1,12 +1,12 @@
 #ifndef PYTHONIC_BUILTIN_SUM_HPP
 #define PYTHONIC_BUILTIN_SUM_HPP
 
-#include "pythonic/utils/proxy.hpp"
-#include "pythonic/utils/int_.hpp"
+#include "pythonic/include/__builtin__/sum.hpp"
 
 #include "pythonic/types/assignable.hpp"
 #include "pythonic/types/tuple.hpp"
-#include "pythonic/include/__builtin__/sum.hpp"
+#include "pythonic/utils/int_.hpp"
+#include "pythonic/utils/proxy.hpp"
 
 #include <algorithm>
 
@@ -15,6 +15,29 @@ namespace pythonic
 
   namespace __builtin__
   {
+
+    namespace details
+    {
+      template <class Tuple>
+      auto tuple_sum(Tuple const &t, utils::int_<0>) -> decltype(std::get<0>(t))
+      {
+        return std::get<0>(t);
+      }
+
+      template <class Tuple>
+      auto tuple_sum(Tuple const &t, utils::int_<1>)
+          -> decltype(std::get<0>(t) + std::get<1>(t))
+      {
+        return std::get<0>(t) + std::get<1>(t);
+      }
+
+      template <class Tuple, size_t I>
+      auto tuple_sum(Tuple const &t, utils::int_<I>)
+          -> decltype(std::get<I>(t) + tuple_sum(t, utils::int_<I - 1>()))
+      {
+        return std::get<I>(t) + tuple_sum(t, utils::int_<I - 1>());
+      }
+    }
 
     template <class Iterable, class T>
     auto sum(Iterable s, T start) -> decltype(std::accumulate(
@@ -34,24 +57,11 @@ namespace pythonic
       return sum(s, 0L);
     }
 
-    template <class Tuple>
-    auto tuple_sum(Tuple const &t, utils::int_<0>) -> decltype(std::get<0>(t))
-    {
-      return std::get<0>(t);
-    }
-
-    template <class Tuple, size_t I>
-    auto tuple_sum(Tuple const &t, utils::int_<I>) ->
-        typename std::remove_reference<decltype(std::get<I>(t))>::type
-    {
-      return std::get<I>(t) + tuple_sum(t, utils::int_<I - 1>());
-    }
-
     template <class... Types>
     auto sum(std::tuple<Types...> const &t)
-        -> decltype(tuple_sum(t, utils::int_<sizeof...(Types)-1>()))
+        -> decltype(details::tuple_sum(t, utils::int_<sizeof...(Types)-1>()))
     {
-      return tuple_sum(t, utils::int_<sizeof...(Types)-1>());
+      return details::tuple_sum(t, utils::int_<sizeof...(Types)-1>());
     }
 
     PROXY_IMPL(pythonic::__builtin__, sum);
--- pythran/pythonic/include/__builtin__/sum.hpp
+++ pythran/pythonic/include/__builtin__/sum.hpp
@@ -1,11 +1,10 @@
 #ifndef PYTHONIC_INCLUDE_BUILTIN_SUM_HPP
 #define PYTHONIC_INCLUDE_BUILTIN_SUM_HPP
 
-#include "pythonic/utils/proxy.hpp"
-#include "pythonic/utils/int_.hpp"
-
-#include "pythonic/types/assignable.hpp"
-#include "pythonic/types/tuple.hpp"
+#include "pythonic/include/types/assignable.hpp"
+#include "pythonic/include/types/tuple.hpp"
+#include "pythonic/include/utils/int_.hpp"
+#include "pythonic/include/utils/proxy.hpp"
 
 #include <algorithm>
 
@@ -14,6 +13,22 @@ namespace pythonic
 
   namespace __builtin__
   {
+
+    namespace details
+    {
+      template <class Tuple>
+      auto tuple_sum(Tuple const &t, utils::int_<0>)
+          -> decltype(std::get<0>(t));
+
+      // FIXME : Why do we need this? compiler doesn't succeed without it...
+      template <class Tuple>
+      auto tuple_sum(Tuple const &t, utils::int_<1>)
+          -> decltype(std::get<0>(t) + std::get<1>(t));
+
+      template <class Tuple, size_t I>
+      auto tuple_sum(Tuple const &t, utils::int_<I>)
+          -> decltype(std::get<I>(t) + tuple_sum(t, utils::int_<I - 1>()));
+    }
 
     template <class Iterable, class T>
     auto sum(Iterable s, T start) -> decltype(std::accumulate(
@@ -24,16 +39,9 @@ namespace pythonic
     template <class Iterable>
     auto sum(Iterable s) -> decltype(sum(s, 0L));
 
-    template <class Tuple>
-    auto tuple_sum(Tuple const &t, utils::int_<0>) -> decltype(std::get<0>(t));
-
-    template <class Tuple, size_t I>
-    auto tuple_sum(Tuple const &t, utils::int_<I>) ->
-        typename std::remove_reference<decltype(std::get<I>(t))>::type;
-
     template <class... Types>
     auto sum(std::tuple<Types...> const &t)
-        -> decltype(tuple_sum(t, utils::int_<sizeof...(Types)-1>()));
+        -> decltype(details::tuple_sum(t, utils::int_<sizeof...(Types)-1>()));
 
     PROXY_DECL(pythonic::__builtin__, sum);
   }

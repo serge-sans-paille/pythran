b481e8692159cde06980942cf835cc271e0bc186
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2015-06-04 12:47:38 +0200

    Fix lack of support of and / or / not... keywords in MSVC

---
+++ pythran/pythonic/compat.hpp
@@ -0,0 +1,5 @@
+#ifndef PYTHONIC_COMPAT_HPP
+
+#include <ciso646>
+
+#endif
--- pythran/pythonic/core.hpp
+++ pythran/pythonic/core.hpp
@@ -1,6 +1,8 @@
 #ifndef PYTHONIC_CORE_HPP
 #define PYTHONIC_CORE_HPP
 
+#include "pythonic/compat.hpp"
+
 #include "pythonic/types/assignable.hpp"
 #include "pythonic/types/combined.hpp"
 #include "pythonic/types/content_of.hpp"

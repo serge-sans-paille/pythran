d0b7282d8354e5060e6ffd7d1dbe791671bc531e
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2015-06-05 10:47:04 +0200

    Make clang-format test more robust

--- pythran/tests/test_format.py
+++ pythran/tests/test_format.py
@@ -1,4 +1,5 @@
 """ Module to check coding style. """
+import glob
 import os
 import subprocess
 
@@ -6,8 +7,8 @@ import subprocess
 def test_clang_format():
     """ Check coding style for cpp files. """
     pythran_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    for root, _, files in os.walk(os.path.join(pythran_dir, "pythonic")):
-        for file_ in files:
+    for root, _, _ in os.walk(os.path.join(pythran_dir, "pythonic")):
+        for file_ in glob.glob(os.path.join(root, "*.hpp")):
             cmd = "clang-format-3.6 {file} | diff -u {file} -".format(
                 file=os.path.join(root, file_))
             process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)

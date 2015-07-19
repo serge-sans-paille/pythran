4528963d42334afecb6f3a557a17e6db3307d77a
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-07-19 21:44:59 +0200

    make sleep more standard

--- pythran/pythonic/include/time/sleep.hpp
+++ pythran/pythonic/include/time/sleep.hpp
@@ -1,8 +1,8 @@
 #ifndef PYTHONIC_INCLUDE_TIME_SLEEP_HPP
 #define PYTHONIC_INCLUDE_TIME_SLEEP_HPP
 
-#include "pythonic/utils/proxy.hpp"
-#include "pythonic/__builtin__/None.hpp"
+#include "pythonic/include/utils/proxy.hpp"
+#include "pythonic/include/types/none.hpp"
 
 namespace pythonic
 {

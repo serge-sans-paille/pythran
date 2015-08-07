88ef27a1162d8bc53b248919e302c0f05f915bbe
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-08-07 08:39:50 +0200

    PEP8 update
    - make some more file pep8 compliant
    - filter out some directories from pep8 checks
    - automate pep8 check in setup.py

--- extensions/pythranmagic.py
+++ extensions/pythranmagic.py
@@ -1,37 +1,42 @@
 """
-Pythran integration into IPython
+Pythran integration into IPython.
 
 * provides the %%pythran magic function to ipython
 """
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Copyright (C) 2010-2011, IPython Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 import imp
 from IPython.core.magic import Magics, magics_class, cell_magic
 
 import pythran
 
+
 @magics_class
 class PythranMagics(Magics):
 
+    """ Class to make it possible to use pythran as a magic IPython command."""
+
     def __init__(self, shell):
-        super(PythranMagics,self).__init__(shell)
+        """ Init the pythran magic stuff. """
+        super(PythranMagics, self).__init__(shell)
         self._reloads = {}
 
     def _import_all(self, module):
-        for k,v in module.__dict__.items():
+        """ Import only globals modules. """
+        for k, v in module.__dict__.items():
             if not k.startswith('__'):
-                self.shell.push({k:v})
+                self.shell.push({k: v})
 
     @cell_magic
     def pythran(self, line, cell):
         """
-        Compile and import everything from a Pythran code cell
+        Compile and import everything from a Pythran code cell.
 
         %%pythran
         #pythran export foo(int)
@@ -39,10 +44,9 @@ class PythranMagics(Magics):
             return x + x
         """
         module_name = "pythranized"
-        module_path  = pythran.compile_pythrancode(module_name, cell)
+        module_path = pythran.compile_pythrancode(module_name, cell)
         module = imp.load_dynamic(module_name, module_path)
         self._import_all(module)
-
 
 
 def load_ipython_extension(ipython):
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class PyTest(TestCommand):
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
+        self.test_args = ['--pep8']
         self.test_suite = True
 
     def run_tests(self):

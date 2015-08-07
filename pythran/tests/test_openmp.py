a59e259adf639701fd546e5d9a2422c64790cebf
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-08-07 09:20:39 +0200

    Improve omp checking for tests
    * And readability for others

--- pythran/tests/test_openmp.py
+++ pythran/tests/test_openmp.py
@@ -5,25 +5,28 @@ import os
 import pythran
 
 class TestOpenMP(TestFromDir):
-    path = os.path.join(os.path.dirname(__file__),"openmp")
+    path = os.path.join(os.path.dirname(__file__), "openmp")
 
 class TestOpenMPLegacy(TestFromDir):
     '''
     Test old style OpenMP constructs, not using comments but strings
     and relying on function-scope locals
     '''
-    path = os.path.join(os.path.dirname(__file__),"openmp.legacy")
+    path = os.path.join(os.path.dirname(__file__), "openmp.legacy")
 
     @staticmethod
     def interface(name, file=None):
-        return { name: [] }
+        return {name: []}
 
 # only activate OpenMP tests if the underlying compiler supports OpenMP
 try:
-    pythran.compile_cxxcode('omp', '#include <omp.h>', extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp'])
+    pythran.compile_cxxcode("omp", '#include <omp.h>',
+                            extra_compile_args=['-fopenmp'],
+                            extra_link_args=['-fopenmp'])
+    import omp
     TestOpenMP.populate(TestOpenMP)
     TestOpenMPLegacy.populate(TestOpenMPLegacy)
-except CompileError:
+except (CompileError, ImportError):
     pass
 
 

3a5eef43c9c1da65502f826012ece3179a146f0f
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2015-10-18 15:43:29 +0200

    Add OSX support to the travis version

--- pythran/tests/travis_test.sh
+++ pythran/tests/travis_test.sh
@@ -1,7 +1,7 @@
-if $PEP8;
+if $PEP8
 then
-  PYTHONPATH=$PWD:$PYTHONPATH py.test pythran --pep8 -m pep8;
+  PYTHONPATH=$PWD:$PYTHONPATH py.test pythran --pep8 -m pep8
 else
-  printf "[compiler]\nCXX=$CXX\nCC=$CC\ncflags=-std=c++11 $CXXFLAGS -w\n" > ~/.pythranrc;
-  OMP_NUM_THREADS=4 PYTHONPATH=$PWD:$PYTHONPATH py.test -n 2 -v $TESTCASE;
+  printf "[compiler]\ncflags=-std=c++11 $CXXFLAGS -w\n" > ~/.pythranrc
+  OMP_NUM_THREADS=4 PYTHONPATH=$PWD:$PYTHONPATH py.test -n 2 -v $TESTCASE
 fi

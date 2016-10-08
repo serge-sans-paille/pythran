05637b07b3d37c56c483a313be9e01f3f31ed1ea
pbrunet <pbrunet@quarkslab.com>
2016-10-08 18:31:08 +0200

    Fix tab in tests.

--- pythran/tests/cases/ccopy.py
+++ pythran/tests/cases/ccopy.py
@@ -13,27 +13,27 @@ def CCOPY(N,CX,INCX,CY,INCY):
 #
 #  =====================================================================
 #
-      	if N <= 0:
-		return
-      	if (INCX==1 and INCY==1):
+    if N <= 0:
+        return
+    if (INCX==1 and INCY==1):
 #
 #        code for both increments equal to 1
 #
-		for I in range(N):
-			CY[I] = CX[I]
-	else:
+        for I in range(N):
+            CY[I] = CX[I]
+    else:
 #
 #        code for unequal increments or equal increments
 #          not equal to 1
 #
-	        IX = 0
-        	IY = 0
-		if (INCX < 0):
-			IX = (-N+1)*INCX
-		if (INCY < 0):
-			IY = (-N+1)*INCY
-		for I in range(N):
-			CY[IY] = CX[IX]
-	            	IX = IX + INCX
-        	    	IY = IY + INCY
-	return
+        IX = 0
+        IY = 0
+        if (INCX < 0):
+            IX = (-N+1)*INCX
+        if (INCY < 0):
+            IY = (-N+1)*INCY
+        for I in range(N):
+            CY[IY] = CX[IX]
+            IX = IX + INCX
+            IY = IY + INCY
+    return

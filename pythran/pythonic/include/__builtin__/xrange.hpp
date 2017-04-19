716b565a02855b2ad387c170a62f926fb301e833
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2017-04-19 22:35:27 +0200

    Remove useless xrange_riterator
    Everything can be done through xrange_iterator

--- pythran/pythonic/include/__builtin__/xrange.hpp
+++ pythran/pythonic/include/__builtin__/xrange.hpp
@@ -27,30 +27,12 @@ namespace pythonic
       long operator-(xrange_iterator const &other) const;
     };
 
-    struct xrange_riterator
-        : std::iterator<std::random_access_iterator_tag, long, ptrdiff_t,
-                        long *, long /*no ref here*/> {
-      long value;
-      long step;
-      long sign;
-      xrange_riterator();
-      xrange_riterator(long v, long s);
-      long operator*();
-      xrange_riterator &operator++();
-      xrange_riterator operator++(int);
-      xrange_riterator &operator+=(long n);
-      bool operator!=(xrange_riterator const &other) const;
-      bool operator==(xrange_riterator const &other) const;
-      bool operator<(xrange_riterator const &other) const;
-      long operator-(xrange_riterator const &other) const;
-    };
-
     struct xrange {
       using value_type = long;
       using iterator = xrange_iterator;
       using const_iterator = xrange_iterator;
-      using reverse_iterator = xrange_riterator;
-      using const_reverse_iterator = xrange_riterator;
+      using reverse_iterator = xrange_iterator;
+      using const_reverse_iterator = xrange_iterator;
 
       long _begin;
       long _end;

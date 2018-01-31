5497dc00cbfdaa1354a51ec09e39f5bc95a36bf7
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-01-31 09:51:12 +0100

    Fix incorrect handling of complex generalized expression
    Fixes #775

---
+++ pythran/tests/cases/weights.py
@@ -0,0 +1,16 @@
+#pythran export weights(uint8[:,:])
+#runas import numpy as np; x = np.arange(10, dtype=np.uint8).reshape(5,2); weights(x)
+
+import numpy as np
+def weights(input_data, threshold=0.3):
+    n_seq, length = input_data.shape
+    weights = np.zeros(n_seq, dtype=np.float32)
+
+    for i in range(n_seq):
+        vector = input_data[i, None, :]
+        count_matches = np.sum(vector == input_data, axis=1)
+        over_threshold = count_matches > (threshold * length)
+        total = np.sum(over_threshold)
+        weights[i] = 1 / total
+
+    return weights

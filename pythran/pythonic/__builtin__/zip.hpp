60650bc11d5669000ae0a655180ca805160d62f4
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-02-25 09:42:32 +0100

    Simpler zip implementation

--- pythran/pythonic/__builtin__/zip.hpp
+++ pythran/pythonic/__builtin__/zip.hpp
@@ -17,27 +17,25 @@ namespace __builtin__
 
   namespace details
   {
-    template <class Iterator0, class... Iterators>
-    auto zip(size_t n, Iterator0 first, Iterators... iters)
-        -> types::list<decltype(types::make_tuple(*first, *iters...))>
+    template <class... Iterators>
+    auto zip(size_t n, Iterators... iters)
+        -> types::list<decltype(types::make_tuple(*iters...))>
     {
-      types::list<decltype(types::make_tuple(*first, *iters...))> out =
+      types::list<decltype(types::make_tuple(*iters...))> out =
           types::empty_list();
       out.reserve(n);
-      for (size_t i = 0; i < n; ++i, ++first, utils::fwd(++iters...))
-        out.push_back(types::make_tuple(*first, *iters...));
+      for (size_t i = 0; i < n; ++i, utils::fwd(++iters...))
+        out.push_back(types::make_tuple(*iters...));
       return out;
     }
   }
 
-  template <class List0, class... Lists>
-  auto zip(List0 &&s0, Lists &&... lists)
-      -> types::list<decltype(types::make_tuple(*s0.begin(),
-                                                *lists.begin()...))>
+  template <class... Lists>
+  auto zip(Lists &&... lists)
+      -> types::list<decltype(types::make_tuple(*lists.begin()...))>
   {
-    size_t n =
-        min(len(std::forward<List0>(s0)), len(std::forward<Lists>(lists))...);
-    return details::zip(n, s0.begin(), lists.begin()...);
+    size_t n = min(len(std::forward<Lists>(lists))...);
+    return details::zip(n, lists.begin()...);
   }
 
   types::empty_list zip()

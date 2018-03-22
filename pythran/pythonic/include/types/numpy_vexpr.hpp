e1d9219096fa3af121d39eb025e1c19d63981d67
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-03-22 12:17:05 +0100

    Better support for array indexing by an other array
    Fixes #825

---
+++ pythran/pythonic/include/types/numpy_vexpr.hpp
@@ -0,0 +1,79 @@
+#ifndef PYTHONIC_INCLUDE_TYPES_NUMPY_VEXPR_HPP
+#define PYTHONIC_INCLUDE_TYPES_NUMPY_VEXPR_HPP
+
+#include "pythonic/include/types/nditerator.hpp"
+
+PYTHONIC_NS_BEGIN
+
+namespace types
+{
+  template <class T, class F>
+  struct numpy_vexpr {
+
+    static constexpr size_t value = value_of<T>::value;
+    static const bool is_vectorizable = false;
+    using dtype = typename dtype_of<T>::type;
+    using value_type = T;
+    static constexpr bool is_strided = T::is_strided;
+
+    using iterator = nditerator<numpy_vexpr>;
+    using const_iterator = const_nditerator<numpy_vexpr>;
+
+    T data_;
+    F view_;
+
+    numpy_vexpr(T const &data, F const &view) : data_{data}, view_{view}
+    {
+    }
+
+    long flat_size() const
+    {
+      auto data_shape = data_.shape();
+      return std::accumulate(data_shape.begin() + 1, data_shape.end(),
+                             view_.shape()[0], std::multiplies<long>());
+    }
+
+    template <class E>
+    numpy_vexpr &operator=(E const &);
+    numpy_vexpr &operator=(numpy_vexpr const &);
+
+    array<long, value> shape() const
+    {
+      array<long, value> res = data_.shape();
+      res.fast(0) = view_.shape()[0];
+      return res;
+    }
+
+    iterator begin();
+    iterator end();
+
+    const_iterator begin() const;
+    const_iterator end() const;
+#ifdef USE_BOOST_SIMD
+    using simd_iterator = const_simd_nditerator<numpy_vexpr>;
+    using simd_iterator_nobroadcast = simd_iterator;
+    template <class vectorizer>
+    simd_iterator vbegin(vectorizer) const;
+    template <class vectorizer>
+    simd_iterator vend(vectorizer) const;
+#endif
+
+    auto fast(long i) -> decltype(data_.fast(i))
+    {
+      return data_.fast(view_.fast(i));
+    }
+    auto fast(long i) const -> decltype(data_.fast(i))
+    {
+      return data_.fast(view_.fast(i));
+    }
+  };
+}
+
+template <class T, class F>
+struct assignable<types::numpy_vexpr<T, F>> {
+  using type = types::ndarray<typename types::dtype_of<T>::type,
+                              types::value_of<T>::value>;
+};
+PYTHONIC_NS_END
+
+#endif

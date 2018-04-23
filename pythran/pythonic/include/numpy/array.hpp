ceb4328b4f380ff8109acf5e6fe19f0b68c04131
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-04-23 08:46:18 +0200

    Simpler numpy.array signature

--- pythran/pythonic/include/numpy/array.hpp
+++ pythran/pythonic/include/numpy/array.hpp
@@ -9,14 +9,9 @@ PYTHONIC_NS_BEGIN
 
 namespace numpy
 {
-  template <
-      class T,
-      class dtype = types::dtype_t<
-          typename utils::nested_container_value_type<typename std::remove_cv<
-              typename std::remove_reference<T>::type>::type>::type>>
-  types::ndarray<typename dtype::type,
-                 utils::nested_container_depth<typename std::remove_cv<
-                     typename std::remove_reference<T>::type>::type>::value>
+  template <class T,
+            class dtype = types::dtype_t<typename std::decay<T>::type::dtype>>
+  types::ndarray<typename dtype::type, std::decay<T>::type::value>
   array(T &&iterable, dtype d = dtype());
 
   DECLARE_FUNCTOR(pythonic::numpy, array);

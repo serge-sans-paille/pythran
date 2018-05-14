d0f2329a6f889af7cb31a29bc58474f3d4c28e9f
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-05-14 22:41:43 +0200

    Update travis testing
    Use less jobs, drop testing SIMD + OpenMP in the process except for
    test_cases.
    
    This should be way faster like this :-)

--- pythran/tests/travis_test.sh
+++ pythran/tests/travis_test.sh
@@ -8,32 +8,11 @@ run_tests() {
         PYTHONPATH=$PWD:$PYTHONPATH py.test pythran/tests/notebooks --nbval
         exit
     fi
-    printf "[compiler]\nCXX=$CXX\nCC=$CC\ncflags=-std=c++11 $CXXFLAGS -w\nldflags=$CXXFLAGS\n" > ~/.pythranrc
-    OMP_NUM_THREADS=4 PYTHONPATH=$PWD:$PYTHONPATH py.test -v $TESTCASE
+    printf "[compiler]\nCXX=$CXX\nCC=$CC\ncflags=-std=c++11 $CXXFLAGS -w -O0 -fvisibility=hidden -fno-wrapv\nldflags=$CXXFLAGS -fopenmp -fvisibility=hidden -Wl,-strip-all\n" > ~/.pythranrc
+    OMP_NUM_THREADS=3 PYTHONPATH=$PWD:$PYTHONPATH py.test -v $TESTCASE
 }
 
 # Test with both clang and gcc
 # Only gcc can test openMP, and travis does not support gcc when gcc uses too much memory
 # So some tests have been commented out, as it is useless to compile them with clang
-if [ -z "$MODE"]
-then
-    CC=clang-3.5 CXX=clang++-3.5 CXXFLAGS="-g" run_tests
-    CC=clang-3.5 CXX=clang++-3.5 CXXFLAGS="-DUSE_BOOST_SIMD -march=native" run_tests
-    CC=gcc-5 CXX=g++-5 CXXFLAGS="-fopenmp" run_tests
-    CC=gcc-5 CXX=g++-5 CXXFLAGS="-DUSE_BOOST_SIMD -march=native -fopenmp" run_tests
-else
-    case $MODE in
-        seq)
-            CC=clang-3.5 CXX=clang++-3.5 CXXFLAGS="-g" run_tests
-            ;;
-        vec)
-            CC=clang-3.5 CXX=clang++-3.5 CXXFLAGS="-DUSE_BOOST_SIMD -march=native" run_tests
-            ;;
-        omp)
-            CC=gcc-5 CXX=g++-5 CXXFLAGS="-fopenmp" run_tests
-            ;;
-        all)
-            CC=gcc-5 CXX=g++-5 CXXFLAGS="-DUSE_BOOST_SIMD -march=native -fopenmp" run_tests
-            ;;
-    esac
-fi
+run_tests

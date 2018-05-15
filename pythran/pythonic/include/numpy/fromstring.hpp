0ae0bf0a4893633d370b0b38c476d9cbf67b6aaf
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-05-15 07:06:37 +0200

    Missing header in numpy/fromstring.hpp

--- pythran/pythonic/include/numpy/fromstring.hpp
+++ pythran/pythonic/include/numpy/fromstring.hpp
@@ -5,6 +5,7 @@
 #include "pythonic/include/numpy/float64.hpp"
 #include "pythonic/include/types/ndarray.hpp"
 #include "pythonic/include/types/list.hpp"
+#include "pythonic/include/types/str.hpp"
 
 #include <limits>
 #include <sstream>
@@ -16,7 +17,7 @@ namespace numpy
   template <class dtype = functor::float64>
   types::ndarray<typename dtype::type, 1>
   fromstring(types::str const &string, dtype d = dtype(), long count = -1,
-             types::str const &sep = "");
+             types::str const &sep = {});
 
   DECLARE_FUNCTOR(pythonic::numpy, fromstring);
 }

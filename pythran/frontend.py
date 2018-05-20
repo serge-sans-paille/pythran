5fddb0806dac5feedb1705e484811e87d04fedda
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-05-20 17:24:56 +0200

    Support OpenMP in imported modules too
    Thanks to Yann Diorcet for spotting that

--- pythran/frontend.py
+++ pythran/frontend.py
@@ -11,21 +11,26 @@ import gast as ast
 import re
 
 
-def parse(pm, code):
+def raw_parse(code):
     # hacky way to turn OpenMP comments into strings
     code = re.sub(r'(\s*)#\s*(omp\s[^\n]+)', r'\1"\2"', code)
 
+    return ast.parse(code)
+
+
+def parse(pm, code):
+
     # front end
-    ir = ast.parse(code)
+    ir = raw_parse(code)
+
+    # Handle user-defined import
+    pm.apply(HandleImport, ir)
 
     # parse openmp directive
     pm.apply(GatherOMPData, ir)
 
     # extract docstrings
     _, docstrings = pm.apply(ExtractDocStrings, ir)
-
-    # Handle user-defined import
-    pm.apply(HandleImport, ir)
 
     # avoid conflicts with cxx keywords
     _, renamings = pm.apply(NormalizeIdentifiers, ir)

16a6d541105dbb412155344caeaa556cdc8473c1
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-07-26 07:44:21 +0200

    Detect common complex number pattern
    abssqr turns |x|^2 into abssqr(x), which is then specialized into:
    
    * x.real^2 + x.imag^2 for complex numbers
    * x * x otherwise
    
    The transformation is always true (brute forced on float32).
    
        #include <math.h>
        #include <stdio.h>
    
        float m0(float x) {
          return sqrt(x) * sqrt(x);
        }
    
        int main() {
          for(unsigned i = 1; i != 0; ++i) {
            float f = *(float*)(void*)&i;
            if(signbit(f)) continue;
    
            float r = m0(f);
            if(f != r && !(isnan(f) && isnan(r)) ){
            printf("%u => %f != %f\n", i, f, r);
          }
          }
          return 0;
        }

---
+++ pythran/tests/cases/make_decision.py
@@ -0,0 +1,12 @@
+import numpy as np
+#pythran export md(complex128[], complex128[::])
+#runas import numpy as np;  s=np.arange(160.)+np.arange(160.)*1.j ; sc = s[::2]; md(s, sc)
+def md(E, symbols):
+    L = E.shape[0]
+    M = symbols.shape[0]
+    syms_out = np.zeros(L, dtype=E.dtype)
+    #omp parallel for
+    for i in range(L):
+        im = np.argmin(abs(E[i]-symbols)**2)
+        syms_out[i] = symbols[im]
+    return syms_out

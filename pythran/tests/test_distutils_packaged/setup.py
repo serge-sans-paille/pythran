7f9f718b44f0ea45c6f423237b90dcf757b6ff4f
Yann Diorcet <yann.diorcet@xenocs.com>
2018-07-30 11:14:52 +0200

    Using PythranBuildExt in setup.py of test units

--- pythran/tests/test_distutils_packaged/setup.py
+++ pythran/tests/test_distutils_packaged/setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup, Extension
-from pythran import PythranExtension
+from pythran.dist import PythranExtension, PythranBuildExt
 
 setup(name = 'demo2',
       version = '1.0',
       description = 'This is another demo package',
       packages = ['demo2'],
+      cmdclass={"build_ext": PythranBuildExt},
       ext_modules = [PythranExtension('demo2.a', sources = ['a.py'])])

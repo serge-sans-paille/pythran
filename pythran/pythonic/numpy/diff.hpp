26c6ff9c45218382d01a1e73781421640608d5af
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-09-18 11:24:20 +0200

    Improve np.diff implementation
    Avoid extra allocation steps.

--- pythran/pythonic/numpy/diff.hpp
+++ pythran/pythonic/numpy/diff.hpp
@@ -11,33 +11,40 @@ PYTHONIC_NS_BEGIN
 
 namespace numpy
 {
+  namespace details
+  {
+    template <class E>
+    types::ndarray<typename E::dtype, types::array<long, E::value>>
+    diff(E const &arr, long n)
+    {
+      auto shape = sutils::array(arr.shape());
+      --shape[E::value - 1];
+      auto out = arr.reshape(
+          shape); // this does not leak, but uses slightly too much memory
 
+      auto slice = std::get<E::value - 1>(arr.shape());
+      auto iter = arr.fbegin();
+      auto out_iter = out.fbegin();
+      for (long i = 0, sz = arr.flat_size(); i < sz; i += slice) {
+        auto prev = *(iter + i);
+        for (long k = 1; k < slice; ++k, ++out_iter) {
+          auto nprev = *(iter + i + k);
+          *(out_iter) = nprev - prev;
+          prev = nprev;
+        }
+      }
+      if (n == 1)
+        return out;
+      else
+        return diff(out, n - 1);
+    }
+  }
   template <class E>
   types::ndarray<typename E::dtype, types::array<long, E::value>>
   diff(E const &expr, long n)
   {
-    auto arr = asarray(expr);
-    auto shape = sutils::array(expr.shape());
-    --shape[E::value - 1];
-
-    types::ndarray<typename E::dtype, types::array<long, E::value>> out(
-        shape, __builtin__::None);
-    auto slice = std::get<E::value - 1>(expr.shape());
-    auto iter = arr.fbegin();
-    auto out_iter = out.fbegin();
-    for (long i = 0, sz = expr.flat_size(); i < sz; i += slice) {
-      auto prev = *(iter + i);
-      for (long k = 1; k < slice; ++k, ++out_iter) {
-        auto nprev = *(iter + i + k);
-        *(out_iter) = nprev - prev;
-        prev = nprev;
-      }
-    }
-    if (n == 1)
-      return out;
-    else
-      return diff(out,
-                  n - 1); // TODO: inplace modification to avoid n-1 allocations
+    return details::diff(array(expr),
+                         n); // that's the only allocation that should happen
   }
 }
 PYTHONIC_NS_END

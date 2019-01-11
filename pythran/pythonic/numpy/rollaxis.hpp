96423e241e25d89b73a4e56963e3ffb243dccaa8
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2019-01-11 22:46:55 +0100

    Better support of numpy.rollaxis

--- pythran/pythonic/numpy/rollaxis.hpp
+++ pythran/pythonic/numpy/rollaxis.hpp
@@ -14,17 +14,25 @@ namespace numpy
   types::ndarray<T, types::array<long, std::tuple_size<pS>::value>>
   rollaxis(types::ndarray<T, pS> const &a, long axis, long start)
   {
-    auto constexpr N = std::tuple_size<pS>::value;
-    if (start >= axis)
-      return copy(a);
+    long constexpr N = std::tuple_size<pS>::value;
     long t[N];
-    for (long i = 0; i < start; ++i)
-      t[i] = i;
-    t[start] = axis;
-    for (long i = start + 1; i <= axis; ++i)
-      t[i] = i - 1;
-    for (long i = axis + 1, n = N; i < n; ++i)
-      t[i] = i;
+    if (start >= axis) {
+      for (long i = 0; i < axis; ++i)
+        t[i] = i;
+      for (long i = axis + 1; i < start; ++i)
+        t[i - 1] = i;
+      t[start - 1] = axis;
+      for (long i = start; i < N; ++i)
+        t[i] = i;
+    } else {
+      for (long i = 0; i < start; ++i)
+        t[i] = i;
+      t[start] = axis;
+      for (long i = start + 1; i <= axis; ++i)
+        t[i] = i - 1;
+      for (long i = axis + 1, n = N; i < n; ++i)
+        t[i] = i;
+    }
     return _transpose(a, t);
   }
 

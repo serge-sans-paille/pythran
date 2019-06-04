ee80d61be2ecf2c7f4d26742287a96def50ce016
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2019-06-04 20:55:06 +0200

    Fix array <> tuple conversion with type casting

--- pythran/pythonic/include/types/NoneType.hpp
+++ pythran/pythonic/include/types/NoneType.hpp
@@ -174,7 +174,7 @@ namespace types
     T &operator=(T const &t);
     intptr_t id() const;
     template <class T1>
-    operator none<T1, true>() const
+    operator none<T1, true>()
     {
       if (is_none)
         return {none_type{}};

5c4ef682f824a954a27529f5ca6f7c09a2bd1e14
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2019-06-17 14:30:09 +0200

    Support kind argument for numpy.sort
    Fix #1064

--- pythran/pythonic/include/numpy/sort.hpp
+++ pythran/pythonic/include/numpy/sort.hpp
@@ -5,6 +5,7 @@
 
 #include "pythonic/include/utils/functor.hpp"
 #include "pythonic/include/types/ndarray.hpp"
+#include "pythonic/include/types/str.hpp"
 
 PYTHONIC_NS_BEGIN
 namespace numpy
@@ -13,6 +14,10 @@ namespace numpy
   template <class E>
   types::ndarray<typename E::dtype, types::array<long, E::value>>
   sort(E const &expr, long axis = -1);
+
+  template <class E>
+  types::ndarray<typename E::dtype, types::array<long, E::value>>
+  sort(E const &expr, long axis, types::str const &kind);
 
   NUMPY_EXPR_TO_NDARRAY0_DECL(sort);
   DEFINE_FUNCTOR(pythonic::numpy, sort);

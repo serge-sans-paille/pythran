d520232a15495a38cb79ec5a1de2e88ac9eb578d
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2019-12-10 09:48:46 +0100

    Fix implicit conversion for unary operator+

--- pythran/pythonic/include/operator_/pos.hpp
+++ pythran/pythonic/include/operator_/pos.hpp
@@ -11,6 +11,10 @@ namespace operator_
   template <class A>
   auto pos(A const &a) -> decltype(+a);
 
+  char pos(char const &a);
+  signed char pos(signed char const &a);
+  unsigned char pos(unsigned char const &a);
+
   DEFINE_FUNCTOR(pythonic::operator_, pos);
 }
 PYTHONIC_NS_END

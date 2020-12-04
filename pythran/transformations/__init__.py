291c464329519ddde50baacb2ae2ed81e30758d4
paugier <pierre.augier@univ-grenoble-alpes.fr>
2020-12-04 11:12:14 +0100

    Basic support for f-strings

--- pythran/transformations/__init__.py
+++ pythran/transformations/__init__.py
@@ -31,3 +31,4 @@ from .remove_lambdas import RemoveLambdas
 from .remove_nested_functions import RemoveNestedFunctions
 from .unshadow_parameters import UnshadowParameters
 from .remove_named_arguments import RemoveNamedArguments
+from .remove_fstrings import RemoveFStrings

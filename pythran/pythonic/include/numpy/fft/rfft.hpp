1cd273854021ff42eee95d612bd07a6e96f43dcc
Jochen Schroeder <jochen.schroeder@gmail.com>
2020-12-23 04:48:03 +0100

    implement fft/ifft using pocketfft
    Based on  pocketfft cpp implemenation git commit 5e9dd4d, licenced under BSD 3-clause "New" licence.

--- pythran/pythonic/include/numpy/fft/rfft.hpp
+++ pythran/pythonic/include/numpy/fft/rfft.hpp
@@ -11,22 +11,59 @@ namespace numpy
   namespace fft
   {
 
-    // I'm sure there's a better way to do this.
-    template <class T, class pS, typename U>
-    types::ndarray<std::complex<typename std::common_type<T, double>::type>,
-                   types::array<long, std::tuple_size<pS>::value>>
-    rfft(types::ndarray<T, pS> const &input, long NFFT = -1, long axis = -1,
-         U renorm = types::str(""));
+    template <class T, class pS>
+    types::ndarray<typename std::enable_if<std::is_floating_point<T>::value, std::complex<T>>::type,
+            types::array<long, std::tuple_size<pS>::value>>
+    rfft(types::ndarray<T, pS> const &a, long n = -1, long axis = -1,
+          types::str const &norm = {});
 
     template <class T, class pS>
-    types::ndarray<std::complex<typename std::common_type<T, double>::type>,
-                   types::array<long, std::tuple_size<pS>::value>>
-    rfft(types::ndarray<T, pS> const &input, long NFFT = -1, long axis = -1);
+    types::ndarray<typename std::enable_if<std::is_floating_point<T>::value, std::complex<T>>::type,
+            types::array<long, std::tuple_size<pS>::value>>
+    rfft(types::ndarray<T, pS> const &a, types::none_type n, long axis,
+          types::str const &norm );
+
+    template <class T, class pS>
+    types::ndarray<typename std::enable_if<std::is_floating_point<T>::value, std::complex<T>>::type,
+            types::array<long, std::tuple_size<pS>::value>>
+    rfft(types::ndarray<T, pS> const &a, long n, long axis,
+          types::none_type norm );
+
+    template <class T, class pS>
+    types::ndarray<typename std::enable_if<std::is_floating_point<T>::value, std::complex<T>>::type,
+            types::array<long, std::tuple_size<pS>::value>>
+    rfft(types::ndarray<T, pS> const &a, types::none_type n , long axis =-1,
+          types::none_type norm = types::none_type{});
+
+    template <class T, class pS>
+    types::ndarray<typename std::enable_if<std::is_integral<T>::value, std::complex<double>>::type,
+            types::array<long, std::tuple_size<pS>::value>>
+    rfft(types::ndarray<T, pS> const &a, long n = -1, long axis = -1,
+          types::str const &norm = {});
+
+    template <class T, class pS>
+    types::ndarray<typename std::enable_if<std::is_integral<T>::value, std::complex<double>>::type,
+            types::array<long, std::tuple_size<pS>::value>>
+    rfft(types::ndarray<T, pS> const &a, types::none_type n, long axis,
+          types::str const &norm );
+
+    template <class T, class pS>
+    types::ndarray<typename std::enable_if<std::is_integral<T>::value, std::complex<double>>::type,
+            types::array<long, std::tuple_size<pS>::value>>
+    rfft(types::ndarray<T, pS> const &a, long n, long axis,
+          types::none_type norm );
+
+    template <class T, class pS>
+    types::ndarray<typename std::enable_if<std::is_integral<T>::value, std::complex<double>>::type,
+            types::array<long, std::tuple_size<pS>::value>>
+    rfft(types::ndarray<T, pS> const &a, types::none_type n , long axis =-1,
+          types::none_type norm = types::none_type{});
 
     NUMPY_EXPR_TO_NDARRAY0_DECL(rfft);
     DEFINE_FUNCTOR(pythonic::numpy::fft, rfft);
   }
 }
 PYTHONIC_NS_END
+
 
 #endif

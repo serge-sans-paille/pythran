e12df26f3b5fde1a0522d99df5542a212454d375
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-01-25 18:36:14 +0100

    Improve copyto support

--- pythran/pythonic/include/numpy/copyto.hpp
+++ pythran/pythonic/include/numpy/copyto.hpp
@@ -7,9 +7,8 @@
 PYTHONIC_NS_BEGIN
 namespace numpy
 {
-  template <class T, class pS0, class pS1>
-  types::ndarray<T, pS0> copyto(types::ndarray<T, pS0> &out,
-                                types::ndarray<T, pS1> const &a);
+  template <class T, class pS, class E>
+  types::ndarray<T, pS> copyto(types::ndarray<T, pS> &out, E const &expr);
 
   DEFINE_FUNCTOR(pythonic::numpy, copyto);
 }
--- pythran/pythonic/numpy/copyto.hpp
+++ pythran/pythonic/numpy/copyto.hpp
@@ -9,11 +9,10 @@
 PYTHONIC_NS_BEGIN
 namespace numpy
 {
-  template <class T, class pS0, class pS1>
-  types::ndarray<T, pS0> copyto(types::ndarray<T, pS0> &out,
-                                types::ndarray<T, pS1> const &a)
+  template <class T, class pS, class E>
+  types::ndarray<T, pS> copyto(types::ndarray<T, pS> &out, E const &expr)
   {
-    std::copy(a.fbegin(), a.fend(), out.fbegin());
+    out[types::contiguous_slice(0, types::none_type{})] = expr;
     return out;
   }
 }

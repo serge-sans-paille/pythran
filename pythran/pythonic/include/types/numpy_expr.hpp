a58b713fb3da83d4edafc4665ff799a41dde0e6c
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-06-16 23:00:34 +0200

    Restore cython testing and fix pythran header accordingly
    This one has been lagging around for too long... And it should be back to normal
    now. Entschuldigung @scoder !
    
    Should fix #1706.

--- pythran/pythonic/include/types/numpy_expr.hpp
+++ pythran/pythonic/include/types/numpy_expr.hpp
@@ -637,6 +637,14 @@ namespace types
     numpy_expr(numpy_expr const &) = default;
     numpy_expr(numpy_expr &&) = default;
 
+#ifdef CYTHON_ABI
+    template <class... Argp>
+    numpy_expr(numpy_expr<Op, Argp...> const &other)
+        : args(other.args)
+    {
+    }
+#endif
+
     numpy_expr(Args const &... args);
 
     template <size_t... I>
--- pythran/tests/test_cython.py
+++ pythran/tests/test_cython.py
@@ -1,33 +1,37 @@
 import os
+import glob
+import sys
 import unittest
 
 class TestCython(unittest.TestCase):
     pass
-# Needs to wait unil cython supports pythran new builtins naming
-#def add_test(name, runner, target):
-#    setattr(TestCython, "test_" + name, lambda s: runner(s, target))
-#
-#try:
-#    import Cython
-#    import glob
-#    import sys
-#    targets = glob.glob(os.path.join(os.path.dirname(__file__), "cython", "setup_*.py"))
-#    sys.path.append(os.path.join(os.path.dirname(__file__), "cython"))
-#
-#    for target in targets:
-#        def runner(self, target):
-#            cwd = os.getcwd()
-#            try:
-#                os.chdir(os.path.dirname(target))
-#                exec(open(os.path.basename(target)).read())
-#            except:
-#                raise
-#            finally:
-#                os.chdir(cwd)
-#        name, _ = os.path.splitext(os.path.basename(target))
-#        add_test(name, runner, target)
-#
-#
-#except ImportError:
-#    pass
+
+def add_test(name, runner, target):
+    setattr(TestCython, "test_" + name, lambda s: runner(s, target))
+
+for intermediate in glob.glob(os.path.join(os.path.dirname(__file__), "cython",
+                                           "*.cpp")):
+    os.remove(intermediate)
+
+try:
+    import Cython
+    targets = glob.glob(os.path.join(os.path.dirname(__file__), "cython", "setup_*.py"))
+    sys.path.append(os.path.join(os.path.dirname(__file__), "cython"))
+
+    for target in targets:
+        def runner(self, target):
+            cwd = os.getcwd()
+            try:
+                os.chdir(os.path.dirname(target))
+                exec(open(os.path.basename(target)).read())
+            except:
+                raise
+            finally:
+                os.chdir(cwd)
+        name, _ = os.path.splitext(os.path.basename(target))
+        add_test(name, runner, target)
+
+
+except ImportError:
+    pass
 

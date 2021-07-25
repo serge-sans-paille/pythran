27a5a562358815a49877ac96d0e44496370bb505
Xingyu Liu <charlotteliu12x@gmail.com>
2021-07-25 23:45:03 +0800

    Introduce support for immediate operand
    Currently hardcode the behavior for `numpy.mean` to explore design space.
    Basically extend the `immediates` analysis to support a wider range of
    configuration instead of just `make_shape`

--- pythran/pythonic/numpy/mean.hpp
+++ pythran/pythonic/numpy/mean.hpp
@@ -2,7 +2,9 @@
 #define PYTHONIC_NUMPY_MEAN_HPP
 
 #include "pythonic/include/numpy/mean.hpp"
-
+#include "pythonic/utils/functor.hpp"
+#include "pythonic/numpy/asarray.hpp"
+#include "pythonic/numpy/expand_dims.hpp"
 #include "pythonic/numpy/sum.hpp"
 #include "pythonic/builtins/None.hpp"
 
@@ -10,19 +12,74 @@ PYTHONIC_NS_BEGIN
 
 namespace numpy
 {
+  template <class E>
+  auto mean(E const &expr, types::none_type axis, types::none_type d,
+            types::none_type out, std::false_type keepdims)
+      -> decltype(sum(expr) / double(expr.flat_size()))
+  {
+    return sum(expr) / double(expr.flat_size());
+  }
+
   template <class E, class dtype>
-  auto mean(E const &expr, types::none_type axis, dtype d)
+  auto mean(E const &expr, types::none_type axis, dtype d, types::none_type out,
+            std::false_type keepdims)
       -> decltype(sum(expr) / typename dtype::type(expr.flat_size()))
   {
     return sum(expr) / typename dtype::type(expr.flat_size());
   }
 
-  template <class E, class dtype>
-  auto mean(E const &expr, long axis, dtype d) -> decltype(sum(expr, axis))
+  template <class E>
+  auto mean(E const &expr, long axis, types::none_type d, types::none_type out,
+            std::false_type keepdims) -> decltype(sum(expr, axis))
   {
-    return sum(expr, axis) /=
-           typename dtype::type(sutils::getshape(expr)[axis]);
+    return sum(expr, axis) /= double(sutils::getshape(expr)[axis]);
   }
+
+  // FIXME: need to implement dtype support for numpy::sum / numpy::reduce
+  // template <class E, class dtype>
+  // auto mean(E const &expr, long axis, dtype d, types::none_type out,
+  // std::false_type keepdims)
+  //     -> decltype(sum(expr, axis, d))
+  // {
+  //   return sum(expr, axis, d) /=
+  //          typename dtype::type(sutils::getshape(expr)[axis]);
+  // }
+
+  template <class E>
+  types::ndarray<double, typename details::make_scalar_pshape<E::value>::type>
+  mean(E const &expr, types::none_type axis, types::none_type d,
+       types::none_type out, std::true_type keep_dims)
+  {
+    return {typename details::make_scalar_pshape<E::value>::type(),
+            mean(expr, axis, d, out)};
+  }
+
+  template <class E, class dtype>
+  types::ndarray<typename dtype::type,
+                 typename details::make_scalar_pshape<E::value>::type>
+  mean(E const &expr, types::none_type axis, dtype d, types::none_type out,
+       std::true_type keep_dims)
+  {
+    return {typename details::make_scalar_pshape<E::value>::type(),
+            mean(expr, axis, d, out)};
+  }
+
+  template <class E>
+  auto mean(E const &expr, long axis, types::none_type d, types::none_type out,
+            std::true_type keepdims)
+      -> decltype(expand_dims(mean(expr, axis), axis))
+  {
+    return expand_dims(mean(expr, axis), axis);
+  }
+
+  // FIXME: need to implement dtype support for numpy::sum / numpy::reduce
+  // template <class E, class dtype>
+  // auto mean(E const &expr, long axis, dtype d, types::none_type out,
+  // std::true_type keepdims)
+  //     -> decltype(expand_dims(mean(expr, axis, d), axis))
+  // {
+  //   return expand_dims(mean(expr, axis, d), axis);
+  // }
 }
 PYTHONIC_NS_END
 

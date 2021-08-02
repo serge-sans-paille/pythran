7af9568cd3a252c44ace47167d4254ae6e031a1f
Xingyu Liu <charlotteliu12x@gmail.com>
2021-08-02 16:20:43 +0800

    add tests for numpy mean with keepdims=False

--- pythran/tests/test_numpy_func0.py
+++ pythran/tests/test_numpy_func0.py
@@ -285,6 +285,12 @@ def np_rosen_der(x):
     def test_mean5(self):
         self.run_test("def np_mean5(a): from numpy import mean ; return mean(a, 2)", numpy.array([[[1, 2], [3, 4.]]]), np_mean5=[NDArray[float,:,:,:]])
 
+    def test_mean6(self):
+        self.run_test("def np_mean6(a): from numpy import mean ; return mean(a, 2, keepdims=False)", numpy.array([[[1, 2], [3, 4.]]]), np_mean6=[NDArray[float,:,:,:]])
+
+    def test_mean7(self):
+        self.run_test("def np_mean7(a): from numpy import mean ; return mean(a, keepdims=False)", numpy.array([[[1, 2], [3, 4.]]]), np_mean7=[NDArray[float,:,:,:]])
+
     def test_var0(self):
         self.run_test("def np_var0(a): return a.var()", numpy.array([[1, 2], [3, 4]], dtype=float), np_var0=[NDArray[float,:,:]])
 

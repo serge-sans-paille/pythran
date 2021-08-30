e26ef0f5d42cb6ec822f5616c04f339a65299cae
Adam J. Stewart <ajstewart426@gmail.com>
2021-08-30 15:47:37 -0500

    Environment variable depends on platform

--- omp/__init__.py
+++ omp/__init__.py
@@ -73,9 +73,15 @@ class OpenMP(object):
         # until Python 3.6+, so we explicitly add it.
         # LD_LIBRARY_PATH is used on Linux, while macOS uses DYLD_LIBRARY_PATH
         # and DYLD_FALLBACK_LIBRARY_PATH.
-        paths = os.environ.get('LD_LIBRARY_PATH', '').split(':')
-        paths += os.environ.get('DYLD_LIBRARY_PATH', '').split(':')
-        paths += os.environ.get('DYLD_FALLBACK_LIBRARY_PATH', '').split(':')
+        env_vars = []
+        if sys.platform == 'darwin':
+            env_vars = ['DYLD_LIBRARY_PATH', 'DYLD_FALLBACK_LIBRARY_PATH']
+        elif sys.platform.startswith('linux'):
+            env_vars = ['LD_LIBRARY_PATH']
+
+        paths = []
+        for env_var in env_vars:
+            paths += os.environ.get(env_var, '').split(os.pathsep)
 
         libomp_names = self.get_libomp_names()
         for libomp_name in libomp_names:
@@ -115,9 +121,15 @@ class OpenMP(object):
                 self.version = 45
                 return
 
-        raise ImportError("I can't find a shared library for libomp,"
-                          " you may need to install it or adjust the "
-                          "LD_LIBRARY_PATH environment variable.")
+        msg = "I can't find a shared library for libomp, you may need to install it"
+        if env_vars:
+            msg += " or adjust the "
+            msg += " or ".join(env_vars)
+            msg += " environment variable"
+            if len(env_vars) > 1:
+                msg += "s"
+        msg += "."
+        raise ImportError(msg)
 
     def __getattr__(self, name):
         """

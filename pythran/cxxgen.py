7781f9ba57cf737d1ba9c14dbdb00e7ec8036573
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-09-08 07:29:21 +0200

    Always add the inline keyword on generated methods/function
    Performance-wise it's just an hint, but it seems to affect a few benchmarks and
    doesn't hurt...
    
    It actually makes https://github.com/scikit-image/scikit-image/pull/3226 faster.

--- pythran/cxxgen.py
+++ pythran/cxxgen.py
@@ -123,11 +123,14 @@ class Typedef(DeclSpecifier):
 class FunctionDeclaration(NestedDeclarator):
     def __init__(self, subdecl, arg_decls, *attributes):
         NestedDeclarator.__init__(self, subdecl)
+        self.inline = True
         self.arg_decls = arg_decls
         self.attributes = attributes
 
     def get_decl_pair(self):
         sub_tp, sub_decl = self.subdecl.get_decl_pair()
+        if self.inline:
+            sub_tp = ['inline'] + sub_tp
         return sub_tp, ("%s(%s) %s" % (
             sub_decl,
             ", ".join(ad.inline() for ad in self.arg_decls),

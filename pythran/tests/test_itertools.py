bd0dd3067698b1ec20d62d772d91b4663af86d85
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2022-02-26 23:43:05 +0100

    Register itertools.permutations second parameter as accepting immediate
    That way we can have a specialization that returns actual tuple instead of
    dynamic tuples.
    
    Fix #1965

--- pythran/tests/test_itertools.py
+++ pythran/tests/test_itertools.py
@@ -143,6 +143,16 @@ def ifilter_with_nested_lambdas(N):
         self.run_test("def permutations_g(l0,a): from itertools import permutations; return sum(map(lambda t : t[0]*t[1], permutations((y for x in l0 for y in range(x)),a)))", [0,1,2], 2, permutations_g=[List[int],int])
 
     def test_next_permutations(self):
+        self.run_test("def next_permutations(n, p):"
+                      "  from itertools import permutations ;"
+                      "  x = permutations(n,p) ;"
+                      "  next(x) ;"
+                      "  return list(map(str, x))",
+                      list(range(5)),
+                      2,
+                      next_permutations=[List[int], int])
+
+    def test_next_permutations_static(self):
         self.run_test("def next_permutations(n):"
                       "  from itertools import permutations ;"
                       "  x = permutations(n,2) ;"

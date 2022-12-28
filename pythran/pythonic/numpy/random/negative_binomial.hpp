86ba39cdbf2b087adf859b95bf22aa44735b9189
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2022-12-28 19:19:35 +0100

    Fix negative binomial implementation
    Use regular C++ support to implement it instead of pretending to be
    smart.

--- pythran/pythonic/numpy/random/negative_binomial.hpp
+++ pythran/pythonic/numpy/random/negative_binomial.hpp
@@ -1,16 +1,16 @@
 #ifndef PYTHONIC_NUMPY_RANDOM_NEGATIVE_BINOMIAL_HPP
 #define PYTHONIC_NUMPY_RANDOM_NEGATIVE_BINOMIAL_HPP
 
-#include "pythonic/include/numpy/random/negative_binomial.hpp"
 #include "pythonic/include/numpy/random/generator.hpp"
+#include "pythonic/include/numpy/random/negative_binomial.hpp"
 
-#include "pythonic/types/ndarray.hpp"
 #include "pythonic/types/NoneType.hpp"
+#include "pythonic/types/ndarray.hpp"
 #include "pythonic/types/tuple.hpp"
 #include "pythonic/utils/functor.hpp"
 
-#include <random>
 #include <algorithm>
+#include <random>
 
 PYTHONIC_NS_BEGIN
 namespace numpy
@@ -19,32 +19,29 @@ namespace numpy
   {
 
     template <class pS>
-    types::ndarray<double, pS> negative_binomial(double n, double p,
-                                                 pS const &shape)
+    types::ndarray<long, pS> negative_binomial(long n, double p,
+                                               pS const &shape)
     {
-      types::ndarray<double, pS> result{shape, types::none_type()};
-      std::gamma_distribution<double> distribution_gamma{n, (1 - p) / p};
-      std::generate(result.fbegin(), result.fend(), [&]() {
-        return std::poisson_distribution<long>{
-            (distribution_gamma(details::generator))}(details::generator);
-      });
+      types::ndarray<long, pS> result{shape, types::none_type()};
+      std::negative_binomial_distribution<long> distribution{n, p};
+      std::generate(result.fbegin(), result.fend(),
+                    [&]() { return distribution(details::generator); });
       return result;
     }
 
-    auto negative_binomial(double n, double p, long size)
+    auto negative_binomial(long n, double p, long size)
         -> decltype(negative_binomial(n, p, types::array<long, 1>{{size}}))
     {
       return negative_binomial(n, p, types::array<long, 1>{{size}});
     }
 
-    double negative_binomial(double n, double p, types::none_type d)
+    long negative_binomial(long n, double p, types::none_type d)
     {
-      std::gamma_distribution<double> distribution_gamma{n, (1 - p) / p};
-      return std::poisson_distribution<long>{
-          (distribution_gamma(details::generator))}(details::generator);
+      std::negative_binomial_distribution<long> distribution{n, p};
+      return distribution(details::generator);
     }
-  }
-}
+  } // namespace random
+} // namespace numpy
 PYTHONIC_NS_END
 
 #endif

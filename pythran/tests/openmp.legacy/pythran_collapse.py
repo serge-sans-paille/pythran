7e1fea8b770602307ff6f08fc66a906d43b8a9e3
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2023-02-01 23:02:12 +0100

    Do not generate trailing loop update if not needed
    Because of the way looping works in python and C, we generally turn
    
        for i in range(10):
            ...
    
    into
    
        for(int i = 0; i < 10; ++i)
           ...
        if ( i != 10) i = 10
    
    Don't do that if we can prove i is not escaping the loop's scope.
    
    Fix #2064

--- pythran/tests/openmp.legacy/pythran_collapse.py
+++ pythran/tests/openmp.legacy/pythran_collapse.py
@@ -1,5 +1,5 @@
 import numpy as np
-def collapse_bug(x, m, btx):
+def collapse_bug0(x, m, btx):
     n = int(np.log2(m))
     lv = np.zeros((x.shape[0], n))
     N = x.shape[0]
@@ -9,8 +9,19 @@ def collapse_bug(x, m, btx):
             lv[i,k] = abs(x[i]- btx[k])
     return lv
 
+def collapse_bug1(x, m, btx):
+    n = int(np.log2(m))
+    lv = np.zeros((x.shape[0], n))
+    N = x.shape[0]
+    #omp parallel for collapse(2) private(i,k)
+    for i in range(N):
+        for k in range(n):
+            lv[i,k] = abs(x[i]- btx[k])
+    return lv
+
+
 def pythran_collapse():
     from random import randint
     n = randint(50, 50)
     x = np.ones(n)
-    return collapse_bug(x, 5, 3. * x)
+    return collapse_bug0(x, 5, 3. * x), collapse_bug1(x, 5, 3. * x)

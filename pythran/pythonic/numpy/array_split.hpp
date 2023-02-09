c308b9b1f66cc85769e585dc3972daa5ad34c3cd
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2023-02-09 13:23:19 +0100

    Improve and optimize numpy.array_split implementation
    Accept any array expression as array split parameter, which avoid a
    useless copy.
    
    Keep sharing between the parameter and the return value, instead of
    creating deep copies.
    
    Perform a fast_contiguous_slice, because it's valid to do so.

--- pythran/pythonic/numpy/array_split.hpp
+++ pythran/pythonic/numpy/array_split.hpp
@@ -3,57 +3,61 @@
 
 #include "pythonic/include/numpy/array_split.hpp"
 
-#include "pythonic/utils/functor.hpp"
 #include "pythonic/types/ndarray.hpp"
+#include "pythonic/utils/functor.hpp"
 
 PYTHONIC_NS_BEGIN
 
 namespace numpy
 {
-  template <class T, class pS>
-  types::list<types::ndarray<T, types::array<long, std::tuple_size<pS>::value>>>
-  array_split(types::ndarray<T, pS> const &a, long nb_split)
+  template <class E>
+  types::list<typename assignable<
+      decltype(std::declval<E>()[types::fast_contiguous_slice()])>::type>
+  array_split(E const &a, long nb_split)
   {
-    long sz = std::distance(a.begin(), a.end());
+    long sz = a.template shape<0>();
     long n = (sz + nb_split - 1) / nb_split;
     long end = n * nb_split;
     long nb_full_split = nb_split;
     if (end != sz)
       nb_full_split -= (end - sz);
-    types::list<types::ndarray<
-        T, types::array<long, std::tuple_size<pS>::value>>> out(nb_split);
+    types::list<typename assignable<
+        decltype(std::declval<E>()[types::fast_contiguous_slice()])>::type>
+        out(nb_split);
+
     long index = 0;
     for (long i = 0; i < nb_full_split; ++i, index += n)
-      out[i] = a[types::contiguous_slice(index, index + n)];
+      out[i] = a[types::fast_contiguous_slice(index, index + n)];
     for (long i = nb_full_split; i < nb_split; ++i, index += (n - 1))
-      out[i] = a[types::contiguous_slice(index, index + n - 1)];
+      out[i] = a[types::fast_contiguous_slice(index, index + n - 1)];
 
     return out;
   }
 
-  template <class T, class pS, class I>
+  template <class E, class I>
   typename std::enable_if<
       types::is_iterable<I>::value,
-      types::list<types::ndarray<
-          T, types::array<long, std::tuple_size<pS>::value>>>>::type
-  array_split(types::ndarray<T, pS> const &a, I const &split_mask)
+      types::list<typename assignable<
+          decltype(std::declval<E>()[types::fast_contiguous_slice()])>::type>>::
+      type
+      array_split(E const &a, I const &split_mask)
   {
-    long sz = std::distance(a.begin(), a.end());
-    types::list<
-        types::ndarray<T, types::array<long, std::tuple_size<pS>::value>>>
+    long sz = a.template shape<0>();
+    types::list<typename assignable<
+        decltype(std::declval<E>()[types::fast_contiguous_slice()])>::type>
         out(1 + split_mask.flat_size());
     long index = 0;
     auto inserter = out.begin();
     for (auto next_index : split_mask) {
-      *inserter++ = a[types::contiguous_slice(index, next_index)];
+      *inserter++ = a[types::fast_contiguous_slice(index, next_index)];
       index = next_index;
     }
-    *inserter = a[types::contiguous_slice(index, sz)];
+    *inserter = a[types::fast_contiguous_slice(index, sz)];
     return out;
   }
 
   NUMPY_EXPR_TO_NDARRAY0_IMPL(array_split);
-}
+} // namespace numpy
 PYTHONIC_NS_END
 
 #endif

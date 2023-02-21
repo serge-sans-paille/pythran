c7c3e2765c9bbea03e1d73a148d90eec9385e393
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2023-02-21 09:01:24 +0100

    Allow more expressions as numpy function parameters
    Fix #2082

--- pythran/pythonic/utils/numpy_conversion.hpp
+++ pythran/pythonic/utils/numpy_conversion.hpp
@@ -8,9 +8,9 @@
 #define NUMPY_EXPR_TO_NDARRAY0_IMPL(fname)                                     \
   template <class E, class... Types,                                           \
             typename std::enable_if<!types::is_ndarray<E>::value &&            \
-                                        types::is_array<E>::value,             \
+                                        types::is_numexpr_arg<E>::value,       \
                                     E>::type * = nullptr>                      \
-  auto fname(E const &expr, Types &&... others)                                \
+  auto fname(E const &expr, Types &&...others)                                 \
   {                                                                            \
     return fname(types::ndarray<typename E::dtype, typename E::shape_t>{expr}, \
                  std::forward<Types>(others)...);                              \
@@ -18,9 +18,9 @@
 #else
 #define NUMPY_EXPR_TO_NDARRAY0_IMPL(fname)                                     \
   template <class E, class... Types>                                           \
-  auto fname(E const &expr, Types &&... others)                                \
+  auto fname(E const &expr, Types &&...others)                                 \
       ->typename std::enable_if<                                               \
-          !types::is_ndarray<E>::value && types::is_array<E>::value,           \
+          !types::is_ndarray<E>::value && types::is_numexpr_arg<E>::value,     \
           decltype(fname(                                                      \
               types::ndarray<typename E::dtype, typename E::shape_t>{expr},    \
               std::forward<Types>(others)...))>::type                          \

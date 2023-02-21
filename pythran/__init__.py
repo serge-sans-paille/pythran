de36286896d51354a21cbeb31a9db70380ad8c60
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2023-02-21 07:56:25 +0100

    Add missing documentation

--- pythran/__init__.py
+++ pythran/__init__.py
@@ -7,6 +7,8 @@ This package provides several entry points
        * compile_cxxcode: c++ (str) to DLL, returns DLL filename
        * compile_pythrancode: python (str) to so/cpp, returns output filename
        * compile_pythranfile: python (file) to so/cpp, returns output filename
+       * import_pythrancode: python (str) to module, returns loaded module
+       * import_pythranfile: python (file) to module, returns loaded module
        * test_compile: passthrough compile test, raises CompileError Exception.
 
 Basic scenario is to turn a Python AST into C++ code:
@@ -29,6 +31,12 @@ Higher level entry points include:
 >>> cpp_file = compile_pythranfile("my_module.py",cpponly=True)
 >>> dll_file = compile_pythrancode("my_module", export + code)
 >>> dll_file = compile_cxxfile("my_module", cpp_file)
+
+It is possible to directly turn pythran code into an imported module:
+>>> code = '#pythran export greet()\\ndef greet(): return "demat"'
+>>> greeter = import_pythrancode(code)
+>>> greeter.greet()
+'demat'
 
 Cleanup
 >>> import os, glob

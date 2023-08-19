bdcebe9151f62d2dc8ad6c08fbd2a95eced7cb31
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2023-08-19 21:41:16 +0200

    Provide support for constant indexing of numpy vexpr
    Fix #2138

--- pythran/tests/test_ndarray.py
+++ pythran/tests/test_ndarray.py
@@ -1066,6 +1066,16 @@ def complex_conversion0(x):
             2 * numpy.arange(10, dtype=numpy.int32).reshape(5,2),
             vexpr0=[List[NDArray[numpy.int32,:,:]], NDArray[numpy.int32,:,:]])
 
+    def test_vexpr1(self):
+        code = '''
+            import numpy as np
+            def vexpr1(a, b):
+                return a[b][0]'''
+        self.run_test(code,
+            numpy.arange(10.),
+            numpy.arange(5, dtype=numpy.int32),
+            vexpr1=[NDArray[float,:], NDArray[numpy.int32,:]])
+
     def test_array_of_pshape(self):
         code = 'def array_of_pshape(x): from numpy import array; return array(x[None].shape)'
         self.run_test(code, numpy.arange(10), array_of_pshape=[NDArray[int,:]])

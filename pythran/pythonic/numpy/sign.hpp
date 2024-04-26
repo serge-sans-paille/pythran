2fb1ee85a3538c469f485c297a4cc9821a80477e
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2024-04-26 07:52:20 +0200

    [Numpy 2.x] Cope with np.sign change on complex
    The new formula is x == 0 ? x : x/abs(x)

--- pythran/pythonic/numpy/sign.hpp
+++ pythran/pythonic/numpy/sign.hpp
@@ -3,8 +3,8 @@
 
 #include "pythonic/include/numpy/sign.hpp"
 
-#include "pythonic/utils/functor.hpp"
 #include "pythonic/types/ndarray.hpp"
+#include "pythonic/utils/functor.hpp"
 #include "pythonic/utils/numpy_traits.hpp"
 
 PYTHONIC_NS_BEGIN
@@ -12,9 +12,9 @@ PYTHONIC_NS_BEGIN
 namespace numpy
 {
 #define NUMPY_NARY_FUNC_NAME sign
-#define NUMPY_NARY_FUNC_SYM xsimd::sign
+#define NUMPY_NARY_FUNC_SYM details::sign
 #include "pythonic/types/numpy_nary_expr.hpp"
-}
+} // namespace numpy
 PYTHONIC_NS_END
 
 #endif

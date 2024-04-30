b37f5842aa22239dec078e4c05427e2f08da5e1f
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2024-04-30 10:54:22 +0200

    Adapt signbit implementation to xsimd update

--- pythran/pythonic/numpy/signbit.hpp
+++ pythran/pythonic/numpy/signbit.hpp
@@ -3,8 +3,8 @@
 
 #include "pythonic/include/numpy/signbit.hpp"
 
-#include "pythonic/utils/functor.hpp"
 #include "pythonic/types/ndarray.hpp"
+#include "pythonic/utils/functor.hpp"
 #include "pythonic/utils/numpy_traits.hpp"
 
 PYTHONIC_NS_BEGIN
@@ -13,9 +13,9 @@ namespace numpy
 {
 
 #define NUMPY_NARY_FUNC_NAME signbit
-#define NUMPY_NARY_FUNC_SYM xsimd::signbit
+#define NUMPY_NARY_FUNC_SYM details::signbit
 #include "pythonic/types/numpy_nary_expr.hpp"
-}
+} // namespace numpy
 PYTHONIC_NS_END
 
 #endif

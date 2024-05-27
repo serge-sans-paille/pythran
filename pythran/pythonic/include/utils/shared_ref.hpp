06d6ac767ab6130536da80949d8cc24842cf1deb
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2024-05-27 23:40:15 +0200

    Fix massive memory leakage in all ndarray
    This is a critical bug, thanks a lot to Tansu Kefeli for raising the
    issue off-list.

--- pythran/pythonic/include/utils/shared_ref.hpp
+++ pythran/pythonic/include/utils/shared_ref.hpp
@@ -60,7 +60,7 @@ namespace utils
     // Uninitialized ctor (rvalue ref)
     shared_ref(no_memory &&) noexcept;
 
-    // Ctor allocate T && forward all arguments to T ctor
+    // Ctor allocate T and forward all arguments to T ctor
     template <class... Types>
     shared_ref(Types &&...args);
 

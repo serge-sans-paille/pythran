3ed0bb72865ffe95cfdaf63059046515f1e47033
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2024-08-07 17:07:46 +0200

    array module support: buffer_info()

--- pythran/pythonic/array/array.hpp
+++ pythran/pythonic/array/array.hpp
@@ -3,27 +3,30 @@
 
 #include "pythonic/include/array/array.hpp"
 
-#include "pythonic/types/list.hpp"
+#include "pythonic/types/array.hpp"
 #include "pythonic/utils/functor.hpp"
 
 PYTHONIC_NS_BEGIN
 
 namespace array
 {
-
-  template <char c>
-  types::array<typename details::typecodes<c>::type>
-  array(std::integral_constant<char, c>)
+  namespace details
   {
-    return {};
-  }
 
-  template <char c, class E>
-  types::array<typename details::typecodes<c>::type>
-  array(std::integral_constant<char, c>, E &&elts)
-  {
-    return {std::forward<E>(elts)};
-  }
+    template <char c>
+    types::array<typename details::typecodes<c>::type>
+    array(std::integral_constant<char, c>)
+    {
+      return {};
+    }
+
+    template <char c, class E>
+    types::array<typename details::typecodes<c>::type>
+    array(std::integral_constant<char, c>, E &&elts)
+    {
+      return {std::forward<E>(elts)};
+    }
+  } // namespace details
 
 } // namespace array
 PYTHONIC_NS_END

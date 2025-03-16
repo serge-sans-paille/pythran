9a21d4813c23c5654730759f300af60fe54b1edf
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2025-03-16 23:29:19 +0100

    Python 3.13 compat: detect removed find_exe

--- pythran/dist.py
+++ pythran/dist.py
@@ -92,7 +92,7 @@ class PythranBuildExtMixIn(object):
                     return find_exe(exe, *args, **kwargs)
 
                 msvc._find_exe = _find_exe
-            except ImportError:
+            except (AttributeError, ImportError):
                 pass
 
         # In general, distutils uses -Wstrict-prototypes, but this option

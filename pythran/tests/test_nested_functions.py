edc8d91e1c9237989cbacf861e92efc9737b62ce
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2025-04-12 12:23:16 +0200

    More flexible implementation of default list
    Still very naive, but that should be a decent start. Fix #2300

--- pythran/tests/test_nested_functions.py
+++ pythran/tests/test_nested_functions.py
@@ -167,3 +167,60 @@ def nonlocal_deep(a):
 
                 inner()'''
         self.run_test(code, 8, nonlocal_shadow_global=[int])
+
+    def test_long_capture_list(self):
+        code = '''
+import numpy
+def long_capture_list():
+    arr = numpy.ones(shape = (3, 20))
+
+    a1 = 1
+    a2 = 2
+    a3 = 3
+    a4 = 4
+    a5 = 5
+    a6 = 6
+    a7 = 7
+    a8 = 8
+    a9 = 9
+    a10 = 10
+
+    a11 = 11
+    a12 = 12
+    a13 = 13
+    a14 = 14
+    a15 = 15
+    a16 = 16
+    a17 = 17
+    a18 = 18
+    a19 = 19
+    a20 = 20
+
+    def save_step():
+        arr[0][:] = [
+            a1,
+            a2,
+            a3,
+            a4,
+            a5,
+            a6,
+            a7,
+            a8,
+            a9,
+            a10,
+            a11,
+            a12,
+            a13,
+            a14,
+            a15,
+            a16,
+            a17,
+            a18,
+            a19,
+            a20,
+        ]
+    save_step()
+    return arr'''
+        self.run_test(code, long_capture_list=[])
+
+

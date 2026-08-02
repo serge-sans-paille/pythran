acf3f57beb1e8f4f5c7c79d696b76a5ec8012fe5
serge-sans-paille <sergesanspaille@free.fr>
2026-08-02 19:24:09 +0200

    Fix combiner for numpy expr of different types
    Fix #2457
    
    Courtesy of @jeanlaroche for the reproducer.

--- pythran/pythonic/include/types/numpy_expr.hpp
+++ pythran/pythonic/include/types/numpy_expr.hpp
@@ -834,8 +834,11 @@ template <class Op, class Op2, class... Args, class... Args2>
 struct __combined<pythonic::types::numpy_expr<Op, Args...>,
                   pythonic::types::numpy_expr<Op2, Args2...>> {
   using type = pythonic::types::ndarray<
-      typename pythonic::types::numpy_expr<Op, Args...>::dtype,
-      pythonic::types::array_tuple<long, pythonic::types::numpy_expr<Op, Args...>::value>>;
+      typename __combined<typename pythonic::types::numpy_expr<Op, Args...>::dtype,
+                          typename pythonic::types::numpy_expr<Op2, Args2...>::dtype>::type,
+      pythonic::types::array_tuple<long,
+                                   std::max(pythonic::types::numpy_expr<Op, Args...>::value,
+                                            pythonic::types::numpy_expr<Op2, Args2...>::value)>>;
 };
 template <class E, class Op, class... Args>
 struct __combined<pythonic::types::numpy_iexpr<E>, pythonic::types::numpy_expr<Op, Args...>> {

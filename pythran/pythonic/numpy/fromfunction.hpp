d58e6ec63dd7d1d6500b8040eec754ee0448fbbd
serge-sans-paille <sergesanspaille@free.fr>
2026-08-03 08:57:30 +0200

    Replace deprecated calls to std::result_of by std::invoke_result

--- pythran/pythonic/numpy/fromfunction.hpp
+++ pythran/pythonic/numpy/fromfunction.hpp
@@ -17,11 +17,11 @@ namespace numpy
 
   template <class F, class dtype, class purity_tag>
   template <class pS>
-  types::ndarray<std::remove_cv_t<std::remove_reference_t<std::result_of_t<F(dtype)>>>, pS>
+  types::ndarray<std::remove_cv_t<std::remove_reference_t<std::invoke_result_t<F, dtype>>>, pS>
   fromfunction_helper<F, 1, dtype, purity_tag>::operator()(F &&f, pS const &shape, dtype d)
   {
-    types::ndarray<std::remove_cv_t<std::remove_reference_t<std::result_of_t<F(dtype)>>>, pS> out(
-        shape, builtins::None);
+    types::ndarray<std::remove_cv_t<std::remove_reference_t<std::invoke_result_t<F, dtype>>>, pS>
+        out(shape, builtins::None);
     long n = out.template shape<0>();
     for (long i = 0; i < n; ++i)
       out[i] = f(i);
@@ -30,10 +30,12 @@ namespace numpy
 
   template <class F, class dtype, class purity_tag>
   template <class pS>
-  types::ndarray<std::remove_cv_t<std::remove_reference_t<std::result_of_t<F(dtype, dtype)>>>, pS>
+  types::ndarray<std::remove_cv_t<std::remove_reference_t<std::invoke_result_t<F, dtype, dtype>>>,
+                 pS>
   fromfunction_helper<F, 2, dtype, purity_tag>::operator()(F &&f, pS const &shape, dtype d)
   {
-    types::ndarray<std::remove_cv_t<std::remove_reference_t<std::result_of_t<F(dtype, dtype)>>>, pS>
+    types::ndarray<std::remove_cv_t<std::remove_reference_t<std::invoke_result_t<F, dtype, dtype>>>,
+                   pS>
         out(shape, builtins::None);
     long n = out.template shape<0>();
     long m = out.template shape<1>();
